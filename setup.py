758a66dbe4d7b63af497b2ba5436e96dabe5525b
Tim Warburton <timwar@box.(none)>
2011-07-22 19:54:49 -0500

    Remove version from islpy dependency.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name="loopy",
         ],
 
       install_requires=[
-          "pyopencl>=2011.1", "pymbolic", "cgen", "islpy>=2011.3"
+          "pyopencl>=2011.1", "pymbolic", "cgen", "islpy"
           ],
 
       author="Andreas Kloeckner",
