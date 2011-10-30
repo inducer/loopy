e3c575ddf10bc27aa3620cb4bff630d420180ad9
Andreas Kloeckner <inform@tiker.net>
2011-10-29 20:29:11 -0400

    Some test shuffling.

--- test/test_loopy.py
+++ test/test_loopy.py
@@ -1,9 +1,6 @@
 from __future__ import division
 
 import numpy as np
-import pyopencl as cl
-import pyopencl.array as cl_array
-import pyopencl.clrandom as cl_random
 import loopy as lp
 
 from pyopencl.tools import pytest_generate_tests_for_pyopencl \
@@ -15,18 +12,14 @@ from pyopencl.tools import pytest_generate_tests_for_pyopencl \
 def test_owed_barriers(ctx_factory):
     dtype = np.float32
     ctx = ctx_factory()
-    order = "C"
-    queue = cl.CommandQueue(ctx,
-            properties=cl.command_queue_properties.PROFILING_ENABLE)
 
     knl = lp.make_kernel(ctx.devices[0],
             "{[i]: 0<=i<100}",
             [
                 "[i:l.0] <float32> z[i] = a[i]"
                 ],
-            [
-                lp.ArrayArg("a", dtype, shape=(100,)),
-                ])
+            [lp.ArrayArg("a", np.float32, shape=(100,))]
+            )
 
     kernel_gen = lp.generate_loop_schedules(knl)
     kernel_gen = lp.check_kernels(kernel_gen)
@@ -39,20 +32,14 @@ def test_owed_barriers(ctx_factory):
 
 
 def test_wg_too_small(ctx_factory):
-    dtype = np.float32
     ctx = ctx_factory()
-    order = "C"
-    queue = cl.CommandQueue(ctx,
-            properties=cl.command_queue_properties.PROFILING_ENABLE)
 
     knl = lp.make_kernel(ctx.devices[0],
             "{[i]: 0<=i<100}",
             [
                 "[i:l.0] <float32> z[i] = a[i]"
                 ],
-            [
-                lp.ArrayArg("a", dtype, shape=(100,)),
-                ],
+            [lp.ArrayArg("a", np.float32, shape=(100,))],
             local_sizes={0: 16})
 
     kernel_gen = lp.generate_loop_schedules(knl)
