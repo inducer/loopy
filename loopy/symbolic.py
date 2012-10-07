346decb55cecee2e3023fd23a24e32937e138314
Andreas Kloeckner <inform@tiker.net>
2012-10-07 12:40:34 -0500

    Fix stringification of LinearSubscript.

--- loopy/symbolic.py
+++ loopy/symbolic.py
@@ -119,6 +119,9 @@ class LinearSubscript(AlgebraicLeaf):
     def __getinitargs__(self):
         return self.aggregate, self.index
 
+    def stringifier(self):
+        return StringifyMapper
+
     mapper_method = intern("map_linear_subscript")
 
 # }}}
