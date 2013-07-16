b00e0888f06d5997694f10d44d0061bc3bca4030
Andreas Kloeckner <inform@tiker.net>
2013-07-16 14:18:35 -0400

    Don't run ref tests on pocl, aborts on (some) image reads

--- loopy/auto_test.py
+++ loopy/auto_test.py
@@ -303,6 +303,10 @@ def _enumerate_cl_devices_for_ref_test():
     from warnings import warn
 
     for pf in cl.get_platforms():
+        if pf.name == "Portable Computing Language":
+            # pocl not mature enough yet, sadly
+            continue
+
         for dev in pf.get_devices():
             if dev.type & cl.device_type.CPU:
                 cpu_devs.append(dev)
