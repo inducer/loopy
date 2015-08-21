033819e54fd65ac8e3196f3ce50bd418ed721a12
Andreas Kloeckner <inform@tiker.net>
2015-08-21 10:05:53 -0500

    Fix wrong import in OpenCL target

--- loopy/target/opencl/__init__.py
+++ loopy/target/opencl/__init__.py
@@ -215,10 +215,10 @@ class OpenCLTarget(CTarget):
     @memoize_method
     def get_dtype_registry(self):
         from loopy.target.c.compyte.dtypes import (DTypeRegistry,
-                fill_registry_with_opencl_types)
+                fill_registry_with_opencl_c_types)
 
         result = DTypeRegistry()
-        fill_registry_with_opencl_types()
+        fill_registry_with_opencl_c_types(result)
 
         # no complex number support--needs PyOpenCLTarget
 
