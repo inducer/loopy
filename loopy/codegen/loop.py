61a833a878798c68d5a9d31e5271fb1d327a5f01
Lucas C Wilcox <lucas@swirlee.com>
2016-01-07 19:33:11 -0600

    Import cleanup

--- loopy/codegen/loop.py
+++ loopy/codegen/loop.py
@@ -1,6 +1,4 @@
-from __future__ import division
-from __future__ import absolute_import
-from six.moves import range
+from __future__ import division, absolute_import
 
 __copyright__ = "Copyright (C) 2012 Andreas Kloeckner"
 
@@ -24,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+from six.moves import range
 
 from loopy.diagnostic import warn, LoopyError
 from loopy.codegen import gen_code_block
