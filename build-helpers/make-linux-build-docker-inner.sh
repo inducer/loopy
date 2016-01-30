0fbb9795a27c6dd09740f03430df72f09927088d
Andreas Kloeckner <inform@tiker.net>
2016-01-29 19:10:20 -0600

    Use Py2.7 on Centos

--- build-helpers/make-linux-build-docker-inner.sh
+++ build-helpers/make-linux-build-docker-inner.sh
@@ -8,7 +8,10 @@ cd /tmp/build
 
 useradd -d /home/user -m -s /bin/bash user
 
-yum install -y git python-devel tar gcc gcc-c++ mercurial numpy libffi-devel
+yum install centos-release-SCL
+yum install -y git python27 python27-python-devel tar gcc gcc-c++ mercurial numpy libffi-devel
+
+PYTHON_EXE=python2.7
 
 VENV_VERSION="virtualenv-1.9.1"
 rm -Rf "$VENV_VERSION"
@@ -19,12 +22,12 @@ $VENV_VERSION/virtualenv.py --system-site-packages --no-setuptools .env
 
 source .env/bin/activate
 
-curl -k https://ssl.tiker.net/software/ez_setup.py | python -
-curl -k https://gitlab.tiker.net/inducer/pip/raw/7.0.3/contrib/get-pip.py | python -
+curl -k https://ssl.tiker.net/software/ez_setup.py | $PYTHON_EXE -
+curl -k https://gitlab.tiker.net/inducer/pip/raw/7.0.3/contrib/get-pip.py | $PYTHON_EXE -
 
 pip install packaging
 
-PYTHON_VER=$(python -c 'import sys; print(".".join(str(s) for s in sys.version_info[:2]))')
+PYTHON_VER=$($PYTHON_EXE -c 'import sys; print(".".join(str(s) for s in sys.version_info[:2]))')
 if test "$PYTHON_VER" = "2.6"; then
   pip install pyinstaller==2.1
 else
@@ -36,7 +39,7 @@ cd loopy
 
 grep -v pyopencl requirements.txt > myreq.txt
 pip install -r myreq.txt
-python setup.py install
+$PYTHON_EXE setup.py install
 
 chown -R user /tmp/build
 
