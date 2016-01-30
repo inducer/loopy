02ee2222d6e37b66bb19dd16bccca02705ed802c
Andreas Kloeckner <inform@tiker.net>
2016-01-29 19:24:35 -0600

    More build fixes

--- build-helpers/make-linux-build-docker-inner.sh
+++ build-helpers/make-linux-build-docker-inner.sh
@@ -11,14 +11,14 @@ useradd -d /home/user -m -s /bin/bash user
 yum install -y centos-release-SCL
 yum install -y git python27 python27-python-devel tar gcc gcc-c++ mercurial numpy libffi-devel
 
-PYTHON_EXE=python2.7
+PYTHON_EXE=python27
 
 VENV_VERSION="virtualenv-1.9.1"
 rm -Rf "$VENV_VERSION"
 curl -k https://pypi.python.org/packages/source/v/virtualenv/$VENV_VERSION.tar.gz | tar xfz -
 
 VIRTUALENV=virtualenv
-$VENV_VERSION/virtualenv.py --system-site-packages --no-setuptools .env
+$VENV_VERSION/virtualenv.py -p $PYTHON_EXE --system-site-packages --no-setuptools .env
 
 source .env/bin/activate
 
@@ -27,7 +27,7 @@ curl -k https://gitlab.tiker.net/inducer/pip/raw/7.0.3/contrib/get-pip.py | $PYT
 
 pip install packaging
 
-PYTHON_VER=$($PYTHON_EXE -c 'import sys; print(".".join(str(s) for s in sys.version_info[:2]))')
+PYTHON_VER=$(python -c 'import sys; print(".".join(str(s) for s in sys.version_info[:2]))')
 if test "$PYTHON_VER" = "2.6"; then
   pip install pyinstaller==2.1
 else
@@ -39,7 +39,7 @@ cd loopy
 
 grep -v pyopencl requirements.txt > myreq.txt
 pip install -r myreq.txt
-$PYTHON_EXE setup.py install
+python setup.py install
 
 chown -R user /tmp/build
 
