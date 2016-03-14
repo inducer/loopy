6d49fcadc7115d4c8d402dd622727c45ce90fab1
Andreas Kloeckner <inform@tiker.net>
2016-03-14 11:21:21 -0500

    Add Python sparse matvec example

---
+++ examples/python/sparse.py
@@ -0,0 +1,18 @@
+import loopy as lp
+
+k = lp.make_kernel([
+    "[m] -> { [i] : 0 <= i < m }",
+    "[length] -> { [j] : 0 <= j < length }"],
+    """
+    rowstart = rowstarts[i]
+    rowend = rowstarts[1 + i]
+    length = rowend + (-1)*rowstart
+    rowsum = 0 {id=zerosum}
+    rowsum = rowsum + x[-1 + colindices[-1 + rowstart + j]]*values[-1 + rowstart + j] {dep=zerosum}
+    y[i] = rowsum
+    """,
+    [
+    lp.GlobalArg("colindices,values,x", shape=None),
+    "..."
+    ])
+print(k)
