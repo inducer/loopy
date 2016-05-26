aa8ae0f8bcb31b78d65e102896a167f1953b6307
Andreas Kloeckner <inform@tiker.net>
2016-05-27 00:54:05 +0200

    Attempt to fix CentOS binary build

--- build-helpers/make-linux-build-docker-inner.sh
+++ build-helpers/make-linux-build-docker-inner.sh
@@ -8,7 +8,7 @@ cd /tmp/build
 
 useradd -d /home/user -m -s /bin/bash user
 
-yum install -y centos-release-SCL
+yum install -y centos-release-scl
 yum install -y git python27 python27-python-devel python27-numpy tar gcc gcc-c++ mercurial libffi-devel
 
 scl enable python27 /mnt/make-linux-build-docker-inner-part-2.sh
