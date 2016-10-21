408ec13487295e60fb719b322b6fad52ec636a2d
Andreas Kloeckner <inform@tiker.net>
2016-10-21 01:51:38 -0500

    Bump data model

--- loopy/version.py
+++ loopy/version.py
@@ -32,4 +32,4 @@ except ImportError:
 else:
     _islpy_version = islpy.version.VERSION_TEXT
 
-DATA_MODEL_VERSION = "v43-islpy%s" % _islpy_version
+DATA_MODEL_VERSION = "v44-islpy%s" % _islpy_version
