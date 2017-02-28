af8325ec7e7275e960d9272daaf74c9e38c7a1a8
Marmaduke Woodman <mmwoodman@gmail.com>
2017-02-28 10:52:24 +0100

    enable batch subscript for temporaries

--- loopy/transform/batch.py
+++ loopy/transform/batch.py
@@ -73,6 +73,9 @@ class _BatchVariableChanger(RuleAwareIdentityMapper):
         if not self.needs_batch_subscript(expr.name):
             return super(_BatchVariableChanger, self).map_variable(expr, expn_state)
 
+        if not hasattr(expr, 'aggregate'):
+            from pymbolic.primitives import Subscript
+            return Subscript(expr, self.batch_iname_expr)
         return expr.aggregate[self.batch_iname_expr]
 
 
