3bd7cdcc9cd612bd8382856348b538fce800aa79
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2017-03-27 21:09:43 +0200

    Introduce kernel option 'check_dep_resolution'
    defaults to True. If set, any unmatched instruction
    dependency will issue an error.

--- loopy/kernel/creation.py
+++ loopy/kernel/creation.py
@@ -1642,7 +1642,7 @@ def _resolve_dependencies(knl, insn, deps):
                     new_deps.append(other_insn.id)
                     found_any = True
 
-        if not found_any:
+        if not found_any and knl.options.check_dep_resolution:
             raise LoopyError("instruction '%s' declared a depency on '%s', "
                     "which did not resolve to any instruction present in the "
                     "kernel '%s'"
