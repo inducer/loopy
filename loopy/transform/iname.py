89ce32dffce7df0717acf35ca1581e640487228d
Andreas Kloeckner <inform@tiker.net>
2017-04-05 18:18:20 -0500

    Error message typo fix

--- loopy/transform/iname.py
+++ loopy/transform/iname.py
@@ -986,8 +986,9 @@ def get_iname_duplication_options(knl, use_boostable_into=False):
             # Emit a warning that we needed boostable_into
             from warnings import warn
             from loopy.diagnostic import LoopyWarning
-            warn("Kernel '%s' required the deprecated 'boostable_into"
-                 "field in order to be schedulable!" % knl.name, LoopyWarning)
+            warn("Kernel '%s' required the deprecated 'boostable_into' "
+                 "instruction attribute in order to be schedulable!" % knl.name,
+                 LoopyWarning)
 
             # Return to avoid yielding the duplication
             # options without boostable_into
