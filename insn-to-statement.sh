286a9c56a26cf07d539be07615bcc60906f30e28
Andreas Kloeckner <inform@tiker.net>
2017-10-17 11:36:02 -0500

    Add all-caps spelling to instruction->stmt replace

--- insn-to-statement.sh
+++ insn-to-statement.sh
@@ -2,6 +2,7 @@ set -x
 set -e
 sed -i s/Instruction/Statement/g $(git ls-files | grep -v compyte)
 sed -i s/instruction/statement/g $(git ls-files | grep -v compyte)
+sed -i s/INSTRUCTION/STATEMENT/g $(git ls-files | grep -v compyte)
 sed -i s/insn/stmt/g $(git ls-files | grep -v compyte)
 for d in kernel codegen transform; do
   git mv loopy/$d/instruction.py loopy/$d/statement.py
