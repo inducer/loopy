4580634a162ff5b50fc5a5f226a2f804e3cf47a0
Kaushik Kulkarni <kaushikcfd@gmail.com>
2017-11-24 14:17:29 -0600

    Some error in `kernel/__init.py__` about `StaticValueFindingError`

--- loopy/kernel/__init__.py
+++ loopy/kernel/__init__.py
@@ -41,7 +41,7 @@ from loopy.library.function import (
 
 from loopy.diagnostic import CannotBranchDomainTree, LoopyError
 from loopy.tools import natsorted
-
+from loopy.diagnostic import StaticValueFindingError
 
 # {{{ unique var names
 
@@ -961,7 +961,7 @@ class LoopKernel(ImmutableRecordWithoutPickling):
                 size = static_max_of_pw_aff(size,
                         constants_only=isinstance(tag, LocalIndexTag),
                         context=self.assumptions)
-            except ValueError:
+            except StaticValueFindingError:
                 pass
 
             tgt_dict[tag.axis] = size
