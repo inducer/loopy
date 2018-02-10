60f59aca1aa3d5960c19e7392a742d2711440974
Nick <nicholas.curtis@uconn.edu>
2018-02-10 16:07:21 -0500

    fix

--- test/test_c_execution.py
+++ test/test_c_execution.py
@@ -310,7 +310,7 @@ def test_missing_compilers():
         knl = lp.fix_parameters(knl, n=n)
         return evalfunc(knl)
 
-    assert __test(lambda knl: lp.generate_code_v2(knl)[0], CTarget)
+    assert __test(lambda knl: lp.generate_code_v2(knl).host_code(), CTarget)
 
     from pytools.prefork import ExecError
 
