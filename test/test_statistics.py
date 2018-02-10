ff36086d2d70d30142d14fdb650b130b94b65a04
Andreas Kloeckner <inform@tiker.net>
2018-02-10 01:37:04 -0600

    Convert left-over language version decl in test_statistics

--- test/test_statistics.py
+++ test/test_statistics.py
@@ -34,7 +34,7 @@ import numpy as np
 from pymbolic.primitives import Variable
 
 
-LOOPY_KERNEL_LANGUAGE_VERSION = (2018, 1)
+from loopy.version import LOOPY_USE_LANGUAGE_VERSION_2018_1  # noqa
 
 
 def test_op_counter_basic():
