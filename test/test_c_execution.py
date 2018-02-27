b7e39fd0cc62abcb68238eb4a730b9edc3fa07f5
Andreas Kloeckner <inform@tiker.net>
2018-02-27 00:24:49 -0600

    Fix pytest script-based test invocation

--- test/test_c_execution.py
+++ test/test_c_execution.py
@@ -347,7 +347,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         exec(sys.argv[1])
     else:
-        from py.test.cmdline import main
+        from pytest import main
         main([__file__])
 
 # vim: foldmethod=marker
