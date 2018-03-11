8ed236525350f164fa40a7947f392d01362df02d
Andreas Kloeckner <inform@tiker.net>
2018-03-11 01:26:38 -0600

    Try hard to find a git revision to use as cache key

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
 
 ver_dic = {}
@@ -10,7 +11,58 @@ try:
 finally:
     version_file.close()
 
+os.environ["AKPYTHON_EXEC_FROM_WITHIN_WITHIN_SETUP_PY"] = "1"
 exec(compile(version_file_contents, "loopy/version.py", 'exec'), ver_dic)
+
+
+# {{{ capture git revision at install time
+
+# authoritative version in pytools/__init__.py
+def find_git_revision(tree_root):
+    # Keep this routine self-contained so that it can be copy-pasted into
+    # setup.py.
+
+    from os.path import join, exists, abspath
+    tree_root = abspath(tree_root)
+
+    if not exists(join(tree_root, ".git")):
+        return None
+
+    from subprocess import Popen, PIPE, STDOUT
+    p = Popen(["git", "rev-parse", "HEAD"], shell=False,
+              stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True,
+              cwd=tree_root)
+    (git_rev, _) = p.communicate()
+
+    import sys
+    if sys.version_info >= (3,):
+        git_rev = git_rev.decode()
+
+    git_rev = git_rev.rstrip()
+
+    retcode = p.returncode
+    assert retcode is not None
+    if retcode != 0:
+        from warnings import warn
+        warn("unable to find git revision")
+        return None
+
+    return git_rev
+
+
+def write_git_revision(package_name):
+    from os.path import dirname, join
+    dn = dirname(__file__)
+    git_rev = find_git_revision(dn)
+
+    with open(join(dn, package_name, "_git_rev.py"), "w") as outf:
+        outf.write("GIT_REVISION = %s\n" % repr(git_rev))
+
+
+write_git_revision("loopy")
+
+# }}}
+
 
 setup(name="loo.py",
       version=ver_dic["VERSION_TEXT"],
@@ -37,7 +89,7 @@ setup(name="loo.py",
           ],
 
       install_requires=[
-          "pytools>=2018.1",
+          "pytools>=2018.2",
           "pymbolic>=2016.2",
           "genpy>=2016.1.2",
           "cgen>=2016.1",
