d8dbcc73966028aa646e7b0d43ba0f396aee9ef8
tj-sun <tianjiao.sun.2010@gmail.com>
2019-01-24 00:54:28 +0000

    do not vectorize reduction over globals

--- loopy/preprocess.py
+++ loopy/preprocess.py
@@ -2489,6 +2489,298 @@ def limit_boostability(kernel):
 # }}}
 
 
+# {{{ realize C vector extension
+
+def realize_c_vec(kernel):
+
+    from loopy.kernel.data import CVectorizeTag, ArrayArg
+    from loopy.kernel.array import CVectorArrayDimTag
+    from loopy.kernel.tools import DomainChanger
+    from loopy.isl_helpers import duplicate_axes
+    from loopy.transform.iname import tag_inames
+    from loopy import Assignment
+    from loopy.symbolic import IdentityMapper, SubstitutionMapper
+    from pymbolic.mapper.substitutor import make_subst_func
+    from pymbolic.primitives import Variable
+
+    # any variable not in subscript?
+    class OutsideVariableFinder(IdentityMapper):
+
+        def __init__(self, find_names):
+            self.result = False
+            self.find_names = find_names
+
+        def map_variable(self, expr, *args, **kwargs):
+            if expr.name in self.find_names:
+                self.result = True
+            return super(OutsideVariableFinder, self).map_variable(
+                expr, args, kwargs)
+
+        def map_subscript(self, expr, *args, **kwargs):
+            return expr
+
+    class VariableFinder(IdentityMapper):
+
+        def __init__(self, find_names, regex=False):
+            self.regex = regex
+            if regex:
+                import re
+                self.find_names = [re.compile(name) for name in find_names]
+            else:
+                self.find_names = find_names
+            self.result = False
+
+        def map_variable(self, expr, *args, **kwargs):
+            if self.regex:
+                for regex in self.find_names:
+                    if regex.match(expr.name):
+                        self.result = True
+                        break
+            elif expr.name in self.find_names:
+                self.result = True
+            return super(VariableFinder, self).map_variable(expr, args, kwargs)
+
+        def map_call(self, expr, *args, **kwargs):
+            if self.regex:
+                for regex in self.find_names:
+                    if regex.match(expr.function.name):
+                        self.result = True
+                        break
+            elif expr.function.name in self.find_names:
+                self.result = True
+            return super(VariableFinder, self).map_call(expr, args, kwargs)
+
+    class SubscriptFinder(IdentityMapper):
+
+        def __init__(self, find_aggregate_name, find_index_names):
+            self.find_aggregate_aname = find_aggregate_name
+            self.find_index_names = find_index_names
+            self.result = False
+
+        def map_subscript(self, expr, *args, **kwargs):
+            if expr.aggregate.name == self.find_aggregate_aname:
+                vf = VariableFinder(self.find_index_names)
+                vf(expr.index)
+                if vf.result:
+                    self.result = True
+            return super(SubscriptFinder, self).map_subscript(expr, args, kwargs)
+
+    cvec_inames = []
+    new_cvec_inames = []
+
+    for i in sorted(kernel.all_inames()):
+        if kernel.iname_tags_of_type(i, CVectorizeTag):
+            cvec_inames.append(i)
+            j = i + "_p"  # TODO: use proper name generator
+            new_cvec_inames.append(j)
+
+            # create domains for new inames
+            domch = DomainChanger(kernel, frozenset([i]))
+            kernel = kernel.copy(domains=domch.get_domains_with(
+                duplicate_axes(domch.domain, [i], [j])))
+
+            # change c_vec to ilp.seq for non-vectorizable instructions
+            kernel = tag_inames(kernel, [(i, "ilp.seq")], retag=True)
+            kernel = tag_inames(kernel, [(j, "c_vec")])
+
+    if not cvec_inames:
+        return kernel
+
+    iname_map = dict(zip(cvec_inames, new_cvec_inames))
+    subst_mapper = SubstitutionMapper(
+        make_subst_func(dict((Variable(o), Variable(n))
+                             for (o, n) in zip(cvec_inames, new_cvec_inames))))
+
+    # TODO: expand this list with more functions
+    func_names = set(["abs_*", "cos_*", "sin_*"])
+    function_finder = VariableFinder(func_names, regex=True)
+    globals = [name for name, arg in kernel.arg_dict.items()
+               if isinstance(arg, ArrayArg) and arg.shape[0] is not None]
+    gbf = VariableFinder(globals)
+
+    new_insts = []
+    for inst in kernel.instructions:
+        if isinstance(inst, Assignment) and (inst.within_inames & set(cvec_inames)):
+            can_vectorize = True
+
+            # reduction over globals are sequentialized
+            if can_vectorize and globals:
+                gbf.result = False
+                gbf(inst.assignee)
+                if gbf.result:
+                    can_vectorize = False
+
+            # some built-in functions cannot be vectorized (in general)
+            if can_vectorize:
+                function_finder.result = False
+                function_finder(inst.expression)
+                if function_finder.result:
+                    can_vectorize = False
+
+            # non privitized array indexed with c_vec iname cannot be vectorized
+            if can_vectorize:
+                for name in inst.dependency_names():
+                    if name in kernel.temporary_variables:
+                        ary = kernel.temporary_variables[name]
+                    elif name in kernel.arg_dict:
+                        ary = kernel.arg_dict[name]
+                        if not isinstance(ary, ArrayArg):
+                            continue
+                    else:
+                        continue
+
+                    if len(ary.dim_tags) < 1:
+                        continue
+
+                    if not isinstance(ary.dim_tags[-1], CVectorArrayDimTag):
+                        sf = SubscriptFinder(ary.name, set(cvec_inames))
+                        sf(inst.assignee)
+                        sf(inst.expression)
+                        if sf.result:
+                            can_vectorize = False
+                            break
+
+            # cannot have inames outside of subscirpt
+            if can_vectorize:
+                ovf = OutsideVariableFinder(
+                    list(inst.within_inames & set(cvec_inames)))
+                ovf(inst.expression)
+                if ovf.result:
+                    can_vectorize = False
+
+            # constant rhs cannot be vectorized (GCC doesn't like it)
+            if can_vectorize:
+                avf = VariableFinder(list(inst.within_inames & set(cvec_inames)))
+                avf(inst.expression)
+                if not avf.result:
+                    import pymbolic.primitives as p
+                    rhs = inst.expression + p.Variable("_zeros")
+                    inst = inst.copy(expression=rhs)
+
+            if can_vectorize:
+                lhs = subst_mapper(inst.assignee)
+                rhs = subst_mapper(inst.expression)
+                within_inames = frozenset(iname_map[i] if i in iname_map else i
+                                          for i in inst.within_inames)
+                inst = inst.copy(assignee=lhs, expression=rhs,
+                                 within_inames=within_inames)
+
+        new_insts.append(inst)
+
+    kernel = kernel.copy(instructions=new_insts)
+    return kernel
+
+# }}}
+
+
+# {{{ find idempotence ("boostability") of instructions
+
+def find_idempotence(kernel):
+    logger.debug("%s: idempotence" % kernel.name)
+
+    writer_map = kernel.writer_map()
+
+    arg_names = set(arg.name for arg in kernel.args)
+
+    var_names = arg_names | set(six.iterkeys(kernel.temporary_variables))
+
+    reads_map = dict(
+            (insn.id, insn.read_dependency_names() & var_names)
+            for insn in kernel.instructions)
+
+    from collections import defaultdict
+    dep_graph = defaultdict(set)
+
+    for insn in kernel.instructions:
+        dep_graph[insn.id] = set(writer_id
+                for var in reads_map[insn.id]
+                for writer_id in writer_map.get(var, set()))
+
+    # Find SCCs of dep_graph. These are used for checking if the instruction is
+    # in a dependency cycle.
+    from loopy.tools import compute_sccs
+
+    sccs = dict((item, scc)
+            for scc in compute_sccs(dep_graph)
+            for item in scc)
+
+    non_idempotently_updated_vars = set()
+
+    new_insns = []
+    for insn in kernel.instructions:
+        boostable = len(sccs[insn.id]) == 1 and insn.id not in dep_graph[insn.id]
+
+        if not boostable:
+            non_idempotently_updated_vars.update(
+                    insn.assignee_var_names())
+
+        new_insns.append(insn.copy(boostable=boostable))
+
+    # {{{ remove boostability from isns that access non-idempotently updated vars
+
+    new2_insns = []
+    for insn in new_insns:
+        if insn.boostable and bool(
+                non_idempotently_updated_vars & insn.dependency_names()):
+            new2_insns.append(insn.copy(boostable=False))
+        else:
+            new2_insns.append(insn)
+
+    # }}}
+
+    return kernel.copy(instructions=new2_insns)
+
+# }}}
+
+
+# {{{ limit boostability
+
+def limit_boostability(kernel):
+    """Finds out which other inames an instruction's inames occur with
+    and then limits boostability to just those inames.
+    """
+
+    logger.debug("%s: limit boostability" % kernel.name)
+
+    iname_occurs_with = {}
+    for insn in kernel.instructions:
+        insn_inames = kernel.insn_inames(insn)
+        for iname in insn_inames:
+            iname_occurs_with.setdefault(iname, set()).update(insn_inames)
+
+    iname_use_counts = {}
+    for insn in kernel.instructions:
+        for iname in kernel.insn_inames(insn):
+            iname_use_counts[iname] = iname_use_counts.get(iname, 0) + 1
+
+    single_use_inames = set(iname for iname, uc in six.iteritems(iname_use_counts)
+            if uc == 1)
+
+    new_insns = []
+    for insn in kernel.instructions:
+        if insn.boostable is None:
+            raise LoopyError("insn '%s' has undetermined boostability" % insn.id)
+        elif insn.boostable:
+            boostable_into = set()
+            for iname in kernel.insn_inames(insn):
+                boostable_into.update(iname_occurs_with[iname])
+
+            boostable_into -= kernel.insn_inames(insn) | single_use_inames
+
+            # Even if boostable_into is empty, leave boostable flag on--it is used
+            # for boosting into unused hw axes.
+
+            insn = insn.copy(boostable_into=boostable_into)
+        else:
+            insn = insn.copy(boostable_into=set())
+
+        new_insns.append(insn)
+
+    return kernel.copy(instructions=new_insns)
+
+# }}}
+
+
 # {{{ check for loads of atomic variables
 
 def check_atomic_loads(kernel):
