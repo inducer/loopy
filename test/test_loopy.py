eb90c823f9c1d7c1ca2c11ccb2f990f08f0af86c
Matt Wala <wala1@illinois.edu>
2019-01-27 15:05:43 -0600

    Revert "Try warnings.simplefilter('always')"
    This reverts commit 3341ab04bd09c261c934175f3fb033cef2a33354.

--- test/test_loopy.py
+++ test/test_loopy.py
@@ -2929,7 +2929,7 @@ def test_backwards_dep_printing_and_error():
 
 def test_temp_var_type_deprecated_usage():
     import warnings
-    warnings.simplefilter("always")
+    warnings.simplefilter("always", DeprecationWarning)
 
     pytest.deprecated_call(lp.Assignment, "x", 1, temp_var_type=lp.auto)
     pytest.deprecated_call(lp.Assignment, "x", 1, temp_var_type=None)
