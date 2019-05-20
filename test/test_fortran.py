7f860cef5d153de796830264d26daaa42081ba90
Andreas Kloeckner <inform@tiker.net>
2019-05-20 18:17:20 -0500

    Adjust var terminology in multi-kernel Fortran test

--- test/test_fortran.py
+++ test/test_fortran.py
@@ -521,8 +521,8 @@ def test_fortran_subroutines(ctx_factory):
 
         end subroutine
         """
-    knl = lp.parse_fortran(fortran_src)
-    print(lp.generate_code_v2(knl).device_code())
+    prg = lp.parse_fortran(fortran_src)
+    print(lp.generate_code_v2(prg).device_code())
 
 
 if __name__ == "__main__":
