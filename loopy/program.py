a42c21f5c98086e30f3616dbe3883e6860694c76
Kaushik Kulkarni <kaushikcfd@gmail.com>
2019-10-14 02:58:02 -0500

    dotted way to call entrypoints

--- loopy/program.py
+++ loopy/program.py
@@ -310,6 +310,13 @@ class Program(ImmutableRecord):
         else:
             return result
 
+    def __getattr__(self, attr):
+        if attr in self.entrypoints:
+            return lambda *args, **kwargs: self(*args, entrypoint=attr,
+                    **kwargs)
+
+        return super(Program, self).__getattr__(attr)
+
     def __call__(self, *args, **kwargs):
         entrypoint = kwargs.get('entrypoint', None)
 
