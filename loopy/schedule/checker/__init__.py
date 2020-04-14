17e14b19e0c59c4854e60b0509ba665b870e468e
jdsteve2 <jdsteve2@illinois.edu>
2020-04-13 23:01:23 -0500

    add docstrings for get_schedule_for_statement_pair() and get_isl_maps_for_LexSchedule()

--- loopy/schedule/checker/__init__.py
+++ loopy/schedule/checker/__init__.py
@@ -20,6 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+# {{{ Create LexSchedule for statement pair
 
 def get_schedule_for_statement_pair(
         knl,
@@ -28,32 +29,52 @@ def get_schedule_for_statement_pair(
         insn_id_after,
         prohibited_var_names=set(),
         ):
-    """A set of dependencies between two statements.
+    """Create a :class:`loopy.schedule.checker.schedule.LexSchedule`
+        representing the order of two statements as a mapping from
+        :class:`loopy.schedule.checker.LexScheduleStatementInstance`
+        to lexicographic time.
 
-    .. arg insn_id_before: An instruction identifier that is unique within
+    :arg knl: A :class:`loopy.kernel.LoopKernel` containing the
+        linearization items that will be used to create a schedule.
+
+    :arg linearization_items: A list of :class:`loopy.schedule.ScheduleItem`
+        (to be renamed to `loopy.schedule.LinearizationItem`) containing
+        the two linearization items for which a schedule will be
+        created. This list may be a partial linearization for a
+        kernel since this function may be used during the linearization
+        process.
+
+    :arg insn_id_before: An instruction identifier that is unique within
         a :class:`loopy.kernel.LoopKernel`.
 
-    .. arg insn_id_after: An instruction identifier that is unique within
+    :arg insn_id_after: An instruction identifier that is unique within
         a :class:`loopy.kernel.LoopKernel`.
 
+    :arg prohibited_var_names: A set of :class:`str` representing
+        variable names that should not be used when creating names for
+        dimensions in a :class:`loopy.schedule.checker.LexSchedule`.
+
+    :returns: A :class:`loopy.schedule.checker.schedule.LexSchedule`
+        representing the order of two statements as a mapping from
+        :class:`loopy.schedule.checker.LexScheduleStatementInstance`
+        to lexicographic time.
     """
 
-    # We don't retrieve linearization items from knl because knl may not be
-    # (fully) linearized yet. This function may be called part way through the
-    # linearization process and receive the current (unfinished) set of
-    # linearization items
-
-    # Preprocess if not already preprocessed
+    # {{{ Preprocess if not already preprocessed
     from loopy import preprocess_kernel
     preproc_knl = preprocess_kernel(knl)
+    # }}}
 
+    # {{{ By default, don't create LexSchedule variables matching existing inames
     if not prohibited_var_names:
         prohibited_var_names = preproc_knl.all_inames()
+    # }}}
 
-    # Get EnterLoop inames tagged as concurrent so LexSchedule can ignore
+    # {{{ Find any EnterLoop inames that are tagged as concurrent
+    # so that LexSchedule knows to ignore them
     # (In the future, this shouldn't be necessary because there
     #  won't be any inames with ConcurrentTags in EnterLoop linearization items.
-    #  Test exercising this: test_linearization_checker_with_stroud_bernstein())
+    #  Test which exercises this: test_linearization_checker_with_stroud_bernstein())
     from loopy.schedule.checker.utils import (
         get_concurrent_inames,
         _get_EnterLoop_inames,
@@ -67,8 +88,9 @@ def get_schedule_for_statement_pair(
             "get_schedule_for_statement_pair encountered EnterLoop for inames %s "
             "with ConcurrentTag(s) in linearization for kernel %s. "
             "Ignoring these loops." % (conc_loop_inames, preproc_knl.name))
+    # }}}
 
-    # Create LexSchedule: mapping of {statement instance: lex point}
+    # {{{ Create LexSchedule: mapping of {statement instance: lex point}
     # include only instructions involved in this dependency
     from loopy.schedule.checker.schedule import LexSchedule
     return LexSchedule(
@@ -78,7 +100,12 @@ def get_schedule_for_statement_pair(
         prohibited_var_names=prohibited_var_names,
         loops_to_ignore=conc_loop_inames,
         )
+    # }}}
 
+# }}}
+
+
+# {{{ Get isl map pair for LexSchedule
 
 def get_isl_maps_for_LexSchedule(
         lex_sched,
@@ -86,19 +113,46 @@ def get_isl_maps_for_LexSchedule(
         insn_id_before,
         insn_id_after,
         ):
-    # Get two isl maps representing the LexSchedule,
-    # one for the 'before' linearization item and one for 'after';
-    # this requires the iname domains
+    """Create a pair of :class:`islpy.Map`s representing a
+        :class:`loopy.schedule.checker.LexSchedule` as two mappings
+        from statement instances to lexicographic time, one for
+        the dependee statement and one for the depender.
 
+    :arg lex_sched: A :class:`loopy.schedule.checker.schedule.LexSchedule`
+        representing the order of two statements as a mapping from
+        :class:`loopy.schedule.checker.LexScheduleStatementInstance`
+        to lexicographic time.
+
+    :arg knl: A :class:`loopy.kernel.LoopKernel` containing the
+        linearization items that will be used to create a schedule.
+
+    :arg insn_id_before: An instruction identifier that is unique within
+        a :class:`loopy.kernel.LoopKernel`.
+
+    :arg insn_id_after: An instruction identifier that is unique within
+        a :class:`loopy.kernel.LoopKernel`.
+
+    :returns: A two-tuple containing two :class:`islpy.Map`s
+        representing the schedule as two mappings
+        from statement instances to lexicographic time, one for
+        the dependee and one for the depender.
+    """
+
+    # {{{ Get iname domains
     insn_before_inames = knl.id_to_insn[insn_id_before].within_inames
     insn_after_inames = knl.id_to_insn[insn_id_after].within_inames
     dom_before = knl.get_inames_domain(insn_before_inames)
     dom_after = knl.get_inames_domain(insn_after_inames)
+    # }}}
 
+    # {{{ Get isl maps
     isl_sched_map_before, isl_sched_map_after = \
         lex_sched.create_isl_maps(
             dom_before,
             dom_after,
         )
+    # }}}
 
     return isl_sched_map_before, isl_sched_map_after
+
+# }}}
