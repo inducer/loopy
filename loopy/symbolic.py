1e2f80a4834002f20c9009cc62693fbac3fbaffe
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2020-06-22 07:15:10 +0100

    Symbolic: Fix handling of Literals.

--- loopy/symbolic.py
+++ loopy/symbolic.py
@@ -361,6 +361,9 @@ class DependencyMapper(DependencyMapperBase):
     def map_resolved_function(self, expr):
         return self.rec(expr.function)
 
+    def map_literal(self, expr):
+        return set()
+
 
 class SubstitutionRuleExpander(IdentityMapper):
     def __init__(self, rules):
@@ -1643,6 +1646,10 @@ class PwAffEvaluationMapper(EvaluationMapperBase, IdentityMapperMixin):
         denom = denom_aff.get_constant_val()
 
         return num.mod_val(denom)
+
+    def map_literal(self, expr):
+        raise TypeError("literal '%s' not supported "
+                        "for as-pwaff evaluation" % expr)
 
 
 def aff_from_expr(space, expr, vars_to_zero=None):
