a5b564b7a627daf09de0003520255b15d3ca0117
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-11-03 02:14:00 -0600

    fix typo

--- loopy/__init__.py
+++ loopy/__init__.py
@@ -282,7 +282,7 @@ __all__ = [
         "TargetBase",
         "CFamilyTarget", "CTarget", "ExecutableCTarget", "generate_header",
         "CudaTarget", "OpenCLTarget",
-        "PyOpenCLTarget", "NvidiaPyOpenCLTarget", "ISPCTarget",
+        "PyOpenCLTarget", "ISPCTarget",
         "NumbaTarget", "NumbaCudaTarget",
         "ASTBuilderBase",
 
