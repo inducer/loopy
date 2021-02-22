7bb3ef2d94d1812a453e8b8fd4261766284bb0ea
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-02-22 15:32:23 -0600

    adds a constant offset as a test

--- test/test_isl.py
+++ test/test_isl.py
@@ -75,15 +75,15 @@ def test_subst_into_pwaff():
     from pymbolic.primitives import Variable
     arg_dict = {
             "m": 3*Variable("nx"),
-            "n": 2*Variable("ny")}
+            "n": 2*Variable("ny")+4}
     space = isl.Set("[nx, ny, nz] -> { []: }").params().space
     poly = isl.PwAff("[m, n] -> { [3 * m + 2 * n] : "
         "m > 0 and n > 0; [7* m + 4*n] : m > 0 and n <= 0 }")
 
     from loopy.isl_helpers import subst_into_pwaff
     result = subst_into_pwaff(space, poly, arg_dict)
-    expected = isl.PwAff("[nx, ny, nz] -> { [(9nx + 4ny)] : nx > 0 and ny > 0;"
-            " [(21nx + 8ny)] : nx > 0 and ny <= 0 }")
+    expected = isl.PwAff("[nx, ny, nz] -> { [(9nx + 4ny+8)] : nx > 0 and ny > -2;"
+            " [(21nx + 8ny+16)] : nx > 0 and ny <= -2 }")
     assert result == expected
 
 
