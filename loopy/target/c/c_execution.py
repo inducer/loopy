216f70dcc301adccecc45bb6e3f6c46d1a11c7d1
Kaushik Kulkarni <kgk2@illinois.edu>
2021-03-06 15:51:15 -0600

    c_execution: array order find: account for ary.shape == ()

--- loopy/target/c/c_execution.py
+++ loopy/target/c/c_execution.py
@@ -92,7 +92,7 @@ class CExecutionWrapperGenerator(ExecutionWrapperGeneratorBase):
                 for i in range(num_axes))
 
         # find order of array
-        order = "'C'" if arg.unvec_strides[-1] == 1 else "'F'"
+        order = "'C'" if (arg.shape == () or arg.unvec_strides[-1] == 1) else "'F'"
 
         gen("%(name)s = _lpy_np.empty(%(shape)s, "
                 "%(dtype)s, order=%(order)s)"
