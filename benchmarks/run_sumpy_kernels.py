007e555444a25b43f585442feee9a92f15920a94
Isuru Fernando <isuruf@gmail.com>
2021-04-23 17:27:10 -0500

    fix caching and add order 12

--- benchmarks/run_sumpy_kernels.py
+++ benchmarks/run_sumpy_kernels.py
@@ -2,11 +2,19 @@ import loopy as lp
 import numpy as np
 import pyopencl as cl
 import logging
+from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
+from pyopencl.tools import (  # noqa
+    pytest_generate_tests_for_pyopencl as pytest_generate_tests,
+)
 
-def _sumpy_kernel_init(dim, order):
+
+def _sumpy_kernel_init(param):
+    name, dim, order = param.name, param.dim, param.order
+    # TODO: add other kernels
+    assert name == "m2l"
     from sumpy.expansion.multipole import (
         LaplaceConformingVolumeTaylorMultipoleExpansion,
     )
@@ -25,11 +33,14 @@ def _sumpy_kernel_init(dim, order):
 
     m2l = E2EFromCSR(ctx, m_expn, l_expn)
     m2l.get_translation_loopy_insns()
+    m2l.ctx = None
+    m2l.device = None
     return m2l
 
 
-def _sumpy_kernel_make(m2l):
-    loopy_knl = m2l.get_optimized_kernel()
+def _sumpy_kernel_make(expn, param):
+    assert param.name == "m2l"
+    loopy_knl = expn.get_optimized_kernel()
     loopy_knl = lp.add_and_infer_dtypes(
         loopy_knl,
         dict(
@@ -48,53 +59,59 @@ def _sumpy_kernel_make(m2l):
     return loopy_knl
 
 
+@dataclass(frozen=True)
+class Param:
+    name: str
+    dim: int
+    order: int
+
+
+def cached_data(params):
+    data = {}
+    np.random.seed(17)
+    logging.basicConfig(level=logging.INFO)
+    for param in params:
+        data[param] = {}
+        expn = _sumpy_kernel_init(param)
+        data[param]["setup"] = expn
+        knl = _sumpy_kernel_make(expn, param)
+        knl = lp.preprocess_kernel(knl)
+        data[param]["instantiated"] = knl
+        scheduled = lp.get_one_scheduled_kernel(knl)
+        data[param]["scheduled"] = scheduled
+    return data
+
+
 class SumpyBenchmarkSuite:
 
-    params = [("m2l", 3, 6)]
+    params = [
+        Param("m2l", dim=3, order=6)
+        Param("m2l", dim=3, order=12)
+    ]
 
     param_names = ["test_name"]
 
     version = 1
 
     def setup_cache(self):
-        data = {}
-        for param in self.params:
-            self.setup(data, param)
-            data[param] = {}
-            m2l = _sumpy_kernel_init(param[1], param[2])
-            data[param]["setup"] = m2l
-            knl = _sumpy_kernel_make(m2l)
-            data[param]["instantiated"] = knl
-            preprocessed = lp.preprocess_kernel(knl)
-            data[param]["preprocessed"] = preprocessed
-            scheduled = lp.get_one_scheduled_kernel(preprocessed)
-            data[param]["scheduled"] = scheduled
-        return data
-
-    def setup(self, data, param):
-        logging.basicConfig(level=logging.INFO)
-        np.random.seed(17)
+        return cached_data(self.params)
 
     def time_instantiate(self, data, param):
-        _sumpy_kernel_make(data[param]["setup"])
-
-    def time_preprocess(self, data, param):
-        lp.preprocess_kernel(data[param]["instantiated"])
+        knl = _sumpy_kernel_make(data[param]["setup"], param)
+        lp.preprocess_kernel(knl)
 
     def time_schedule(self, data, param):
-        lp.get_one_scheduled_kernel(data[param]["preprocessed"])
+        lp.get_one_scheduled_kernel(data[param]["instantiated"])
 
     def time_generate_code(self, data, param):
         lp.generate_code_v2(data[param]["scheduled"])
 
     time_instantiate.timeout = 600.0
-    time_preprocess.timeout = 600.0
     time_schedule.timeout = 600.0
     time_generate_code.timeout = 600.0
 
     # No warmup is needed
     time_instantiate.warmup_time = 0
-    time_preprocess.warmup_time = 0
     time_schedule.warmup_time = 0
     time_generate_code.warmup_time = 0
 
@@ -104,6 +121,5 @@ class SumpyBenchmarkSuite:
 
     # Run memory benchmarks as well
     peakmem_instantiate = time_instantiate
-    peakmem_preprocess = time_preprocess
     peakmem_schedule = time_schedule
     peakmem_generate_code = time_generate_code
