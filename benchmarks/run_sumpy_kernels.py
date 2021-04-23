1a074f5433f765ec1c64fd45ff734bd9e4e8e691
Isuru Fernando <isuruf@gmail.com>
2021-04-23 15:58:29 -0500

    use installed sumpy

--- benchmarks/run_sumpy_kernels.py
+++ benchmarks/run_sumpy_kernels.py
@@ -5,14 +5,51 @@ from pyopencl.tools import (  # noqa
 
 import logging
 logger = logging.getLogger(__name__)
-from .sumpy_kernels import m2l_3d_order_6
 
 import loopy as lp
+
+def _sumpy_kernel_init(dim, order):
+    ctx = ctx_factory()
+    queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
+
+    np.random.seed(17)
+
+    knl = LaplaceKernel(dim)
+    local_expn_class = LaplaceConformingVolumeTaylorLocalExpansion
+    mpole_expn_class = LaplaceConformingVolumeTaylorMultipoleExpansion
+    target_kernels = [knl]
+    m_expn = mpole_expn_class(knl, order=order)
+    l_expn = local_expn_class(knl, order=order)
+
+    from sumpy import P2EFromSingleBox, E2PFromSingleBox, P2P, E2EFromCSR
+    m2l = E2EFromCSR(ctx, m_expn, l_expn)
+    m2l.get_translation_loopy_insns()
+    return m2l
+
+def _sumpy_kernel_make(m2l):
+    loopy_knl = m2l.get_optimized_kernel()
+    loopy_knl = lp.add_and_infer_dtypes(
+        loopy_knl,
+        dict(
+            tgt_ibox=np.int32,
+            centers=np.float64,
+            tgt_center=np.float64,
+            target_boxes=np.int32,
+            src_ibox=np.int32,
+            src_expansions=np.float64,
+            tgt_rscale=np.float64,
+            src_rscale=np.float64,
+            src_box_starts=np.int32,
+            src_box_lists=np.int32,
+        ),
+    )
+    return loopy_knl
+
 
 class SumpyBenchmarkSuite:
 
     params = [
-        "m2l_3d_order_6"
+        ("m2l", 3, 6)
     ]
 
     param_names = ['test_name']
@@ -24,7 +61,9 @@ class SumpyBenchmarkSuite:
         for param in self.params:
             self.setup(data, param)
             data[param] = {}
-            knl = globals()[param]()
+            m2l = _sumpy_kernel_init(param[1], param[2])
+            data[param]["setup"] = m2l
+            knl = _sumpy_kernel_make(m2l)
             data[param]["instantiated"] = knl
             preprocessed = lp.preprocess_kernel(knl)
             data[param]["preprocessed"] = preprocessed
@@ -37,8 +76,7 @@ class SumpyBenchmarkSuite:
         np.random.seed(17)
 
     def time_instantiate(self, data, param):
-        create_knl = globals()[param]
-        create_knl()
+        create_knl = _sumpy_kernel_make(data[param]["setup"])
 
     def time_preprocess(self, data, param):
         lp.preprocess_kernel(data[param]["instantiated"])
