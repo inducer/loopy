75b3d804fb014f59a399bc48c8b03b3417f80f2c
jdsteve2 <jdsteve2@illinois.edu>
2021-04-26 02:38:21 -0500

    create test for loop nest semantics parsing (copied in from old branch (https://gitlab.tiker.net/jdsteve2/loopy/iname-sets-in-loop-priorities))

---
+++ test/test_loop_nest_semantics.py
@@ -0,0 +1,198 @@
+__copyright__ = "Copyright (C) 2021 James Stevens"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import sys
+import loopy as lp
+import pyopencl as cl
+
+import logging
+logger = logging.getLogger(__name__)
+
+try:
+    import faulthandler
+except ImportError:
+    pass
+else:
+    faulthandler.enable()
+
+from pyopencl.tools import pytest_generate_tests_for_pyopencl \
+        as pytest_generate_tests
+
+__all__ = [
+        "pytest_generate_tests",
+        "cl"  # "cl.create_some_context"
+        ]
+
+
+from loopy.version import LOOPY_USE_LANGUAGE_VERSION_2018_2  # noqa
+
+
+def test_loop_constraint_strings_validity_check():
+    ref_knl = lp.make_kernel(
+            "{ [g,h,i,j,k,xx]: 0<=g,h,i,j,k,xx<n }",
+            "out[g,h,i,j,k,xx] = 2*a[g,h,i,j,k,xx]",
+            assumptions="n >= 1",
+            )
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, "{g,h,k},{j,i}")
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, "{g,h,i,k},{j}")
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, "{g,{h,i,k}")
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, "{g,~h,i,k}")
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, "{g,#h,i,k}")
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, ("{g,{h}", "i,k"))
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, ("{g,~h}", "i,k"))
+        assert False
+    except ValueError as e:
+        assert "Unrecognized character(s)" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, ("k", "~{g,h}", "{g,h}"))
+        assert False
+    except ValueError as e:
+        assert "Complement (~) not allowed" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, ("k", "{i,j,k}", "{g,h}"))
+        assert False
+    except ValueError as e:
+        assert "contains cycle" in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_not_nest=("~j,i", "{j,i}"))
+        assert False
+    except ValueError as e:
+        assert ("Complements of sets containing multiple inames "
+            "must enclose inames in braces") in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_nest=("k", "{h}", "{j,i,}"))
+        assert False
+    except ValueError as e:
+        assert ("Found 2 inames but expected 3") in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_nest=("k", "{h}", "{j, x x, i}"))
+        assert False
+    except ValueError as e:
+        assert ("Found 4 inames but expected 3") in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_nest="{h}}")
+        assert False
+    except ValueError as e:
+        assert (
+            "Unrecognized character(s) ['{', '}', '}'] in nest string {h}}"
+            ) in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_nest="{h i j,,}")
+        assert False
+    except ValueError as e:
+        assert(
+            "Unrecognized character(s) [\'{\', \'}\'] in nest string {h i j,,}"
+            ) in str(e)
+
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_nest=("{h}}", "i"))
+        assert False
+    except ValueError as e:
+        assert (
+            "Unrecognized character(s) [\'}\'] in nest string h}"
+            ) in str(e)
+
+    # TODO these should pass
+    """
+    try:
+        lp.constrain_loop_nesting(ref_knl, must_nest=("{h i j,,}", "k"))
+        assert False
+    except ValueError as e:
+        assert("Unrecognized character(s) [\'{\', \'}\'] in nest string {h i j,,}"
+            ) in str(e)
+    """
+
+    # valid syntax
+    lp.constrain_loop_nesting(ref_knl, must_not_nest=("~{j,i}", "{j,i}"))
+    lp.constrain_loop_nesting(ref_knl, must_not_nest=("{h}", "{j,i}"))
+    lp.constrain_loop_nesting(ref_knl, must_not_nest=("h", "{j,i}"))
+    lp.constrain_loop_nesting(ref_knl, must_nest=("k", "{h}", "{j,i}"))
+    lp.constrain_loop_nesting(ref_knl, must_nest=("k", "h", "{j,i}"))
+    lp.constrain_loop_nesting(ref_knl, must_not_nest="~j,j")
+    lp.constrain_loop_nesting(ref_knl, must_nest="k,h,j")
+
+    # handling spaces
+    knl = lp.constrain_loop_nesting(ref_knl, must_nest=("k", "{h }", " { j , i } "))
+    assert list(knl.loop_nest_constraints.must_nest)[0][0].inames == set("k")
+    assert list(knl.loop_nest_constraints.must_nest)[0][1].inames == set("h")
+    assert list(knl.loop_nest_constraints.must_nest)[0][2].inames == set(["j", "i"])
+
+    try:
+        knl = lp.constrain_loop_nesting(ref_knl, ("j", "{}"))
+        assert False
+    except ValueError as e:
+        assert "Found 0 inames" in str(e)
+
+    try:
+        knl = lp.constrain_loop_nesting(ref_knl, ("j", ""))
+        assert False
+    except ValueError as e:
+        assert "Found 0 inames" in str(e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        exec(sys.argv[1])
+    else:
+        from pytest import main
+        main([__file__])
+
+# vim: foldmethod=marker
