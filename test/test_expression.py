28e5f737c6cbdf68f9f6313da9106ebd01451e29
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-04-30 11:51:43 +0200

    Add complex abs test.

--- test/test_expression.py
+++ test/test_expression.py
@@ -501,6 +501,7 @@ def test_complex_support(ctx_factory, target):
             euler1_imag[i] = imag(euler1[i])
             real_times_complex[i] = in1[i]*(in2[i]*1j)
             real_plus_complex[i] = in1[i] + (in2[i]*1j)
+            abs_complex[i] = cabs(real_plus_complex[i])
             complex_div_complex[i] = (2jf + 7*in1[i])/(32jf + 37*in1[i])
             complex_div_real[i] = (2jf + 7*in1[i])/in1[i]
             real_div_complex[i] = in1[i]/(2jf + 7*in1[i])
@@ -533,6 +534,7 @@ def test_complex_support(ctx_factory, target):
     np.testing.assert_allclose(out["euler1_imag"], 0, atol=1e-10)
     np.testing.assert_allclose(out["real_times_complex"], in1*(in2*1j))
     np.testing.assert_allclose(out["real_plus_complex"], in1+(in2*1j))
+    np.testing.assert_allclose(out["abs_complex"], np.sqrt(in1**2+in2**2))
     np.testing.assert_allclose(out["complex_div_complex"], (2j+7*in1)/(32j+37*in1))
     np.testing.assert_allclose(out["complex_div_real"], (2j + 7*in1)/in1)
     np.testing.assert_allclose(out["real_div_complex"], in1/(2j + 7*in1))
