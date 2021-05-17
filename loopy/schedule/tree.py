b791786c0d9e68ccc2e0e0f38f758e1cf837ae58
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-05-17 18:47:45 -0500

    adds v1 of predicate insertion mapper

--- loopy/schedule/tree.py
+++ loopy/schedule/tree.py
@@ -1,7 +1,11 @@
+import pymbolic.primitives as prim
 import loopy.schedule as schedule
 from loopy.diagnostic import LoopyError
-from typing import List, Union, Any, Optional
+from typing import List, Union, Any, Optional, Tuple
 from dataclasses import dataclass, field
+import islpy as isl
+from islpy import dim_type
+from functools import reduce
 
 
 # {{{ LoopKernel.schedule a tree
@@ -90,6 +94,19 @@ class Function(ScheduleNode):
     children: List[Union[InstructionBlock, Loop]]
 
     mapper_method: str = field(default="map_function", repr=False, init=False)
+
+
+class For(Loop):
+    iname: str
+    upper_bound: Union[int, prim.Expression]
+    lower_bound: Union[int, prim.Expression]
+    step: int
+
+    mapper_method: str = field(default="map_resolved_for", repr=False, init=False)
+
+
+class If(ScheduleNode):
+    condition: Union[int, bool, prim.Expression]
 
 
 @dataclass
@@ -303,3 +320,168 @@ class StringifyMapper(CombineMapper):
         return self.combine([f"{self._indent(level)}for {expr.iname}",
                              super().map_function(expr, level+1),
                              f"{self._indent(level)}end {expr.iname}"])
+
+
+def _align_and_intersect(d1, d2):
+    d1, d2 = isl.align_two(d1, d2)
+    return d1 & d2
+
+
+def _wrap_in_if(cond, nodes):
+    if cond.is_universe():
+        return nodes
+    else:
+        return If(cond, nodes)
+
+
+@dataclass(frozen=True)
+class PredicateInsertionContext:
+    implemented_domain: isl.BasicSet
+    gsize: Optional[Tuple[isl.PwAff, ...]] = None
+    lsize: Optional[Tuple[isl.PwAff, ...]] = None
+
+    def copy(self, *, implemented_domain=None, gsize=None, lsize=None):
+        if implemented_domain is None:
+            implemented_domain = self.implemented_domain
+
+        if gsize is None:
+            gsize = self.gsize
+
+        if lsize is None:
+            lsize = self.lsize
+
+        return PredicateInsertionContext(implemented_domain, gsize, lsize)
+
+
+class PredicateInsertion(IdentityMapper):
+    def __init__(self, kernel):
+        self.kernel = kernel
+
+    def map_schedule(self, expr):
+        universe = isl.BasicSet(isl.Space.create_from_names(self.kernel.isl_context,
+                                                            []))
+
+        return super().map_schedule(expr, PredicateInsertionContext(universe))
+
+    def map_function(self, expr, context):
+        # get the implemented domain for the insn ids in this kernel
+        # Shouldn't be difficult to write a combine mapper for it.
+        gsize, lsize = self.kernel.get_grid_sizes_for_insns_ids(
+            gather_insn_ids_for_kernel(expr.name))
+        return super().map_function(expr, context.copy(gsize=gsize, lsize=lsize))
+
+    def map_run_instruction(self, expr, context):
+        return expr
+
+    def map_barrier(self, expr, context):
+        return expr
+
+    def map_instruction_block(self, expr, context):
+        if all(isinstance(child, RunInstruction) for child in expr.children):
+            # need to add a predicate for the hardware axes usage.
+            assert len({self.kernel.id_to_insn[child.insn_id].within_inames
+                        for child in expr.children}) == 1
+            inames = self.kernel.id_to_insn[expr.children[0].insn_id].within_inames
+            hw_inames = inames - set(context.implemented_domain.get_var_dict())
+            if hw_inames:
+                raise NotImplementedError
+
+            return InstructionBlock([self.rec(child, context)
+                                     for child in expr.children])
+        else:
+            assert all(isinstance(child, Barrier) for child in expr.childre)
+            return InstructionBlock([self.rec(child, context)
+                                     for child in expr.children])
+
+    def map_loop(self, expr, context):
+        from loopy.symbolic import aff_to_expr, set_to_cond_expr
+
+        implemented_domain = context.implemented_domain
+        assert implemented_domain.dim(dim_type.set) == 0
+
+        domain = self.kernel.get_iname_domain(expr.iname)
+
+        # {{{ make already implemented loops as parallel; project out inner loops
+
+        for set_dim in domain.get_var_names(dim_type.set):
+            dt, pos = domain.get_var_dict()[set_dim]
+            assert dt == dim_type.set
+
+            if set_dim in implemented_domain.get_var_dict():
+                # make outer loop's iname a param
+                domain = domain.move_dims(dim_type.param,
+                                          domain.dim(dim_type.param),
+                                          dt, pos, 1)
+            elif set_dim != expr.name:
+                domain = domain.project_out(dt, pos, 1)
+            else:
+                pass
+
+        # }}}
+
+        assert domain.dim(dim_type.set) == 1
+
+        domain, implemented_domain = isl.align_two(domain,
+                                                   implemented_domain)
+        domain = domain.gist(implemented_domain)
+
+        downstream_domain = _align_and_intersect(domain
+                                                 .move_dims(dim_type.param,
+                                                            domain.dim(dim_type
+                                                                       .param),
+                                                            dim_type.set,
+                                                            0, 1),
+                                                 implemented_domain)
+
+        outer_condition = isl.align_space(domain.project_out(dim_type.set, 0),
+                                          downstream_domain).gist(downstream_domain)
+
+        min_affs = [aff for set, aff in domain.dim_min(0)]
+        max_affs = [aff for set, aff in domain.dim_max(0)]
+        lower_bound = reduce(isl.Aff.min, min_affs, min_affs[0])
+        upper_bound = reduce(isl.Aff.max, max_affs, max_affs[0])
+
+        inner_condition = domain.affine_hull()
+        step = 1  # TODO: from inner_condition try to guess the step
+
+        children = [self.rec(child, (context
+                                     .copy(implemented_domain=downstream_domain)))
+                    for child in expr.children]
+
+        return _wrap_in_if(outer_condition,
+                           For(aff_to_expr(lower_bound),
+                               aff_to_expr(upper_bound),
+                               step,
+                               _wrap_in_if(set_to_cond_expr(inner_condition),
+                                           children)))
+
+
+class InstructionGatherer(CombineMapper):
+    """
+    Mapper to gather all insn ids a :class:`Function`.
+    """
+    def __init__(self, function_name):
+        self.function_name = function_name
+
+    def combine(self, values):
+        assert all(isinstance(value, list) for value in values)
+        return sum(values, [])
+
+    def map_function(self, expr):
+        if expr.name == self.function_name:
+            return super().map_function(expr)
+        else:
+            return []
+
+    def map_run_instruction(self, expr):
+        return [expr.insn_id]
+
+    def map_barrier(self, expr):
+        if expr.originating_insn_id is not None:
+            return [expr.originating_insn_id]
+        else:
+            return []
+
+
+def gather_insn_ids_for_kernel(schedule, kernel_name):
+    return InstructionGatherer(kernel_name)(schedule)
