cd76f66e29dfa043c26ac96ae9ee953b917bd6c1
jdsteve2 <jdsteve2@illinois.edu>
2021-07-23 15:49:37 -0500

    add a test that tries adding a must_not_nest constraint that conflicts with a vec tag

--- test/test_nest_constraints.py
+++ test/test_nest_constraints.py
@@ -218,68 +218,68 @@ def test_loop_nest_constraints_satisfied():
         ]
 
     loop_nests = set([("g", "h", "i", "j", "k"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, must_nest_constraints, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("g", "i", "h", "j", "k"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, must_nest_constraints, must_not_nest_constraints)
-    assert not valid
+    assert not satisfied
 
     loop_nests = set([("g", "h", "i", "k", "j"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, must_nest_constraints, must_not_nest_constraints)
-    assert not valid
+    assert not satisfied
 
     # now j, k must be innermost
     must_not_nest_constraints = [
         process_loop_nest_specification(("{k,j}", "~{k,j}")),
         ]
     loop_nests = set([("g", "i", "h", "j", "k"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("g", "h", "i", "k", "j"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("g", "i", "j", "h", "k"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert not valid
+    assert not satisfied
 
     loop_nests = set([("g", "h", "j", "k", "i"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert not valid
+    assert not satisfied
 
     loop_nests = set([("j", "k"), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("g", "k"), ])  # j not present
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("g", "i"), ])  # j, k not present
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("k",), ])  # only k present
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
     loop_nests = set([("i",), ])
-    valid = loop_nest_constraints_satisfied(
+    satisfied = loop_nest_constraints_satisfied(
         loop_nests, None, must_not_nest_constraints)
-    assert valid
+    assert satisfied
 
 # }}}
 
@@ -384,9 +384,10 @@ def test_incompatible_nest_constraints():
 def test_vec_innermost():
 
     def is_innermost(iname, lin_items):
+        """Return True if iname is nested innermost"""
         from loopy.schedule import (EnterLoop, LeaveLoop)
 
-        # find EnterLoop(iname) in linearization
+        # Find EnterLoop(iname) in linearization
         enter_iname_idx = None
         for i, lin_item in enumerate(lin_items):
             if isinstance(lin_item, EnterLoop) and (
@@ -394,20 +395,19 @@ def test_vec_innermost():
                 enter_iname_idx = i
                 break
         else:
-            # iname not found
+            # Iname not found
             return False
 
-        # now go through remaining linearization items after EnterLoop(iname)
+        # Now check linearization items after EnterLoop(iname)
+        # and before LeaveLoop(iname) to see if another loop is entered
         for lin_item in lin_items[enter_iname_idx+1:]:
             if isinstance(lin_item, LeaveLoop):
-                # Break as soon as we find a LeaveLoop
-                # If this happens before we find an EnterLoop, iname is innermost
-                break
+                # We found a LeaveLoop *before* finding an EnterLoop, so
+                # iname is innermost
+                return True
             elif isinstance(lin_item, EnterLoop):
-                # we found an EnterLoop inside iname
+                # We found an EnterLoop inside iname
                 return False
-
-        return True
 
     ref_knl = lp.make_kernel(
             "{ [g,h,i,j,k]: 0<=g,h,i,j,k<n }",
@@ -441,7 +441,7 @@ def test_vec_innermost():
     assert is_innermost("h", lin_knl.linearization)
     lp.set_caching_enabled(True)
 
-    # try adding a must_nest constraint that conflicts with a vec tag
+    # Add a must_nest constraint that conflicts with a vec tag
     knl = ref_knl
     knl = lp.tag_inames(knl, {"h": "vec"})
     try:
@@ -452,32 +452,30 @@ def test_vec_innermost():
             "iname h tagged with ConcurrentTag, "
             "cannot use iname in must-nest constraint" in str(e))
 
-    # try adding a vec tag that conflicts with a must_nest constraint
-    # TODO uncomment after implemented in tag_inames
-    """
+    # Add a must_not_nest constraint that conflicts with a vec tag
+    # and attempt to linearize
     knl = ref_knl
-    knl = lp.constrain_loop_nesting(knl, must_nest=("{g,h,i,j}", "{k}"))
+    knl = lp.tag_inames(knl, {"h": "vec"})
+    knl = lp.constrain_loop_nesting(knl, must_not_nest=("{j}", "{h}"))
     try:
-        lp.tag_inames(knl, {"h": "vec"})
+        proc_prog = preprocess_kernel(knl)
+        get_one_linearized_kernel(
+            proc_prog["loopy_kernel"],
+            proc_prog.callables_table,
+            debug_args={"interactive": False},
+            )
         raise AssertionError()
-    except ValueError as e:
-        assert (
-            "cannot tag 'h' as concurrent--iname involved "
-            "in must-nest constraint" in str(e))
-    """
-
-    # TODO try adding a vec tag forcing h to be innermost, but
-    # also add a must-not-nest constraint preventing h
-    # from nesting inside j
+    except RuntimeError as e:
+        assert "no valid schedules found" in str(e)
 
 # }}}
 
 
-# {{{ test_linearization_with_nesting_constraints
+# {{{ test_linearizaztion_with_nesting_constraints
 
 def test_linearization_with_nesting_constraints():
 
-    def loop_order(lin_items):
+    def order_of_enterloops(lin_items):
         from loopy.schedule import EnterLoop
         order = []
         for lin_item in lin_items:
@@ -494,14 +492,15 @@ def test_linearization_with_nesting_constraints():
             )
     ref_knl = lp.add_and_infer_dtypes(ref_knl, {"a": np.dtype(np.float32)})
 
-    # must_nest constraints
+    # {{{ must_nest constraints
+
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
         knl,
         must_nest=("i", "j", "h", "k", "g"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert loop_order(lin_knl.linearization) == ["i", "j", "h", "k", "g"]
+    assert order_of_enterloops(lin_knl.linearization) == ["i", "j", "h", "k", "g"]
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -509,7 +508,7 @@ def test_linearization_with_nesting_constraints():
         must_nest=("k", "{g, h, i, j}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert loop_order(lin_knl.linearization)[0] == "k"
+    assert order_of_enterloops(lin_knl.linearization)[0] == "k"
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -517,7 +516,7 @@ def test_linearization_with_nesting_constraints():
         must_nest=("{g, h, i, j}", "k"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert loop_order(lin_knl.linearization)[-1] == "k"
+    assert order_of_enterloops(lin_knl.linearization)[-1] == "k"
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -525,7 +524,7 @@ def test_linearization_with_nesting_constraints():
         must_nest=("{g, h, i}", "{j, k}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert set(loop_order(lin_knl.linearization)[-2:]) == set(["j", "k"])
+    assert set(order_of_enterloops(lin_knl.linearization)[-2:]) == set(["j", "k"])
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -537,9 +536,9 @@ def test_linearization_with_nesting_constraints():
         must_nest=("i", "{g, h}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert set(loop_order(lin_knl.linearization)[3:]) == set(["j", "k"])
-    assert set(loop_order(lin_knl.linearization)[1:3]) == set(["g", "h"])
-    assert loop_order(lin_knl.linearization)[0] == "i"
+    assert set(order_of_enterloops(lin_knl.linearization)[3:]) == set(["j", "k"])
+    assert set(order_of_enterloops(lin_knl.linearization)[1:3]) == set(["g", "h"])
+    assert order_of_enterloops(lin_knl.linearization)[0] == "i"
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -547,11 +546,13 @@ def test_linearization_with_nesting_constraints():
         must_nest=("i", "{g, h}", "{j, k}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert set(loop_order(lin_knl.linearization)[3:]) == set(["j", "k"])
-    assert set(loop_order(lin_knl.linearization)[1:3]) == set(["g", "h"])
-    assert loop_order(lin_knl.linearization)[0] == "i"
+    assert set(order_of_enterloops(lin_knl.linearization)[3:]) == set(["j", "k"])
+    assert set(order_of_enterloops(lin_knl.linearization)[1:3]) == set(["g", "h"])
+    assert order_of_enterloops(lin_knl.linearization)[0] == "i"
 
-    # must_not_nest constraints
+    # }}}
+
+    # {{{ must_not_nest constraints
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -559,7 +560,7 @@ def test_linearization_with_nesting_constraints():
         must_not_nest=("~k", "k"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert loop_order(lin_knl.linearization)[0] == "k"
+    assert order_of_enterloops(lin_knl.linearization)[0] == "k"
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -567,7 +568,7 @@ def test_linearization_with_nesting_constraints():
         must_not_nest=("k", "~k"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert loop_order(lin_knl.linearization)[-1] == "k"
+    assert order_of_enterloops(lin_knl.linearization)[-1] == "k"
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -575,7 +576,7 @@ def test_linearization_with_nesting_constraints():
         must_not_nest=("{j, k}", "~{j, k}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert set(loop_order(lin_knl.linearization)[-2:]) == set(["j", "k"])
+    assert set(order_of_enterloops(lin_knl.linearization)[-2:]) == set(["j", "k"])
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -587,11 +588,14 @@ def test_linearization_with_nesting_constraints():
         must_nest=("i", "{g, h}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert set(loop_order(lin_knl.linearization)[3:]) == set(["j", "k"])
-    assert set(loop_order(lin_knl.linearization)[1:3]) == set(["g", "h"])
-    assert loop_order(lin_knl.linearization)[0] == "i"
+    assert set(order_of_enterloops(lin_knl.linearization)[3:]) == set(["j", "k"])
+    assert set(order_of_enterloops(lin_knl.linearization)[1:3]) == set(["g", "h"])
+    assert order_of_enterloops(lin_knl.linearization)[0] == "i"
 
-    # must_nest + must_not_nest
+    # }}}
+
+    # {{{ must_nest and must_not_nest
+
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
         knl,
@@ -599,9 +603,9 @@ def test_linearization_with_nesting_constraints():
         must_not_nest=("i", "{g, h}"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert set(loop_order(lin_knl.linearization)[3:]) == set(["j", "k"])
-    assert set(loop_order(lin_knl.linearization)[0:2]) == set(["g", "h"])
-    assert loop_order(lin_knl.linearization)[2] == "i"
+    assert set(order_of_enterloops(lin_knl.linearization)[3:]) == set(["j", "k"])
+    assert set(order_of_enterloops(lin_knl.linearization)[0:2]) == set(["g", "h"])
+    assert order_of_enterloops(lin_knl.linearization)[2] == "i"
 
     knl = ref_knl
     knl = lp.constrain_loop_nesting(
@@ -609,7 +613,7 @@ def test_linearization_with_nesting_constraints():
         must_not_nest=("i", "~i"),
         )
     lin_knl = _process_and_linearize(knl)
-    assert loop_order(lin_knl.linearization)[-1] == "i"
+    assert order_of_enterloops(lin_knl.linearization)[-1] == "i"
 
     # contradictory must_not_nest
 
@@ -633,6 +637,8 @@ def test_linearization_with_nesting_constraints():
         raise AssertionError()
     except RuntimeError as e:
         assert "no valid schedules found" in str(e)
+
+    # }}}
 
 # }}}
 
