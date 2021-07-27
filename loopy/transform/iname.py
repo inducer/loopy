c10349ff753542e3430983bb11d9811f690c4e6f
jdsteve2 <jdsteve2@illinois.edu>
2021-07-27 18:40:43 -0500

    raise NotImplementedError in rename_iname when there are nest_constraints and does_exist=True

--- loopy/transform/iname.py
+++ loopy/transform/iname.py
@@ -2223,6 +2223,17 @@ def rename_iname(kernel, old_iname, new_iname, existing_ok=False, within=None):
                 "--cannot rename" % new_iname)
 
     if does_exist:
+
+        # FIXME implement loop nest constraint updating when does_exist=True
+        # (otherwise constraints handled in duplicate_iname and remove_unused_inames)
+        if kernel.loop_nest_constraints and (
+                kernel.loop_nest_constraints.must_nest or
+                kernel.loop_nest_constraints.must_not_nest or
+                kernel.loop_nest_constraints.must_nest_graph):
+            raise NotImplementedError(
+                "rename_iname() does not yet handle new loop nest "
+                "constraints when does_exist=True.")
+
         # {{{ check that the domains match up
 
         dom = kernel.get_inames_domain(frozenset((old_iname, new_iname)))
