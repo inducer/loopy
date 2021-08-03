f223889ae66c6513ce32ecc68bc3876920a70a80
jdsteve2 <jdsteve2@illinois.edu>
2021-08-03 18:07:48 -0500

    test usage of new dep DAG during linearization (copy in changes from old cartoon-dep-graph branch)

--- test/test_linearization_checker.py
+++ test/test_linearization_checker.py
@@ -148,6 +148,13 @@ def _process_and_linearize(knl, knl_name="loopy_kernel"):
         proc_knl[knl_name], proc_knl.callables_table)
     return lin_knl.linearization, proc_knl[knl_name], lin_knl
 
+
+def _get_runinstruction_ids_from_linearization(lin_items):
+    from loopy.schedule import RunInstruction
+    return [
+        lin_item.insn_id for lin_item in lin_items
+        if isinstance(lin_item, RunInstruction)]
+
 # }}}
 
 
@@ -1576,10 +1583,7 @@ def test_sios_with_matmul():
     lin_items, proc_knl, lin_knl = _process_and_linearize(knl)
 
     # Get ALL statement id pairs
-    from loopy.schedule import RunInstruction
-    all_stmt_ids = [
-        lin_item.insn_id for lin_item in lin_items
-        if isinstance(lin_item, RunInstruction)]
+    all_stmt_ids = _get_runinstruction_ids_from_linearization(lin_items)
     from itertools import product
     stmt_id_pairs = []
     for idx, sid in enumerate(all_stmt_ids):
@@ -1980,6 +1984,120 @@ def test_filtering_deps_by_same():
         }
 
     assert filtered_depends_on_dict == depends_on_dict_expected
+
+# }}}
+
+
+# {{{ test_linearization_using_simplified_dep_graph
+
+def test_linearization_using_simplified_dep_graph():
+    # Test use of simplified dep graph during linearization.
+    # The deps created below should yield a simplified dep graph that causes the
+    # linearization process to order assignments below in numerical order
+
+    # Make a kernel
+    knl = lp.make_kernel(
+        "{[i,j,k,m] : 0 <= i,j,k,m < n}",
+        """
+        for i,j,k,m
+            <>t5 = 5 {id=s5}
+            <>t3 = 3 {id=s3}
+            <>t4 = 4 {id=s4}
+            <>t1 = 1 {id=s1}
+            <>t2 = 2 {id=s2}
+        end
+        """)
+    knl = lp.tag_inames(knl, "m:l.0")
+
+    stmt_ids_ordered_desired = ["s1", "s2", "s3", "s4", "s5"]
+
+    # {{{ Add some deps
+
+    def _dep_with_condition(stmt_before, stmt_after, cond):
+        sid_after = 0 if stmt_before == stmt_after else 1
+        return _isl_map_with_marked_dims(
+            "[n] -> {{"
+            "[{0}'=0, i', j', k', m'] -> [{0}={1}, i, j, k, m] : "
+            "0 <= i,j,k,m,i',j',k',m' < n and {2}"
+            "}}".format(
+                STATEMENT_VAR_NAME, sid_after, cond))
+
+    # Should NOT create an edge:
+    dep_s2_on_s1_1 = _dep_with_condition(2, 1, "i'< i and j'<=j and k' =k and m'=m")
+    # Should create an edge:
+    dep_s2_on_s1_2 = _dep_with_condition(2, 1, "i'<=i and j'<=j and k' =k and m'=m")
+    # Should NOT create an edge:
+    dep_s2_on_s2_1 = _dep_with_condition(2, 2, "i'< i and j'<=j and k' =k and m'=m")
+    # Should NOT create an edge:
+    dep_s2_on_s2_2 = _dep_with_condition(2, 2, "i'<=i and j'<=j and k'< k and m'=m")
+    # Should create an edge:
+    dep_s3_on_s2_1 = _dep_with_condition(3, 2, "i'<=i and j'<=j and k' =k and m'=m")
+    # Should create an edge:
+    dep_s4_on_s3_1 = _dep_with_condition(4, 3, "i'<=i and j'<=j and k' =k and m'=m")
+    # Should create an edge:
+    dep_s5_on_s4_1 = _dep_with_condition(5, 4, "i' =i and j' =j and k' =k and m'=m")
+
+    knl = lp.add_dependency_v2(knl, "s2", "s1", dep_s2_on_s1_1)
+    knl = lp.add_dependency_v2(knl, "s2", "s1", dep_s2_on_s1_2)
+    knl = lp.add_dependency_v2(knl, "s2", "s2", dep_s2_on_s2_1)
+    knl = lp.add_dependency_v2(knl, "s2", "s2", dep_s2_on_s2_2)
+    knl = lp.add_dependency_v2(knl, "s3", "s2", dep_s3_on_s2_1)
+    knl = lp.add_dependency_v2(knl, "s4", "s3", dep_s4_on_s3_1)
+    knl = lp.add_dependency_v2(knl, "s5", "s4", dep_s5_on_s4_1)
+
+    # }}}
+
+    # {{{ Test filteringn of deps by intersection with SAME
+
+    from loopy.schedule.checker.dependency import (
+        filter_deps_by_intersection_with_SAME,
+    )
+    filtered_depends_on_dict = filter_deps_by_intersection_with_SAME(
+        knl["loopy_kernel"])
+
+    # Make sure filtered edges are correct
+
+    # (m is concurrent so shouldn't matter)
+    depends_on_dict_expected = {
+        "s2": set(["s1"]),
+        "s3": set(["s2"]),
+        "s4": set(["s3"]),
+        "s5": set(["s4"]),
+        }
+
+    assert filtered_depends_on_dict == depends_on_dict_expected
+
+    # }}}
+
+    # {{{ Get a linearization WITHOUT using the simplified dep graph
+
+    knl = lp.set_options(knl, use_dependencies_v2=False)
+    lin_items, proc_knl, lin_knl = _process_and_linearize(knl)
+
+    # Check stmt order (should be wrong)
+    stmt_ids_ordered = _get_runinstruction_ids_from_linearization(lin_items)
+    assert stmt_ids_ordered != stmt_ids_ordered_desired
+
+    # Check dep satisfaction (should not all be satisfied)
+    unsatisfied_deps = lp.find_unsatisfied_dependencies(proc_knl, lin_items)
+    assert unsatisfied_deps
+
+    # }}}
+
+    # {{{ Get a linearization using the simplified dep graph
+
+    knl = lp.set_options(knl, use_dependencies_v2=True)
+    lin_items, proc_knl, lin_knl = _process_and_linearize(knl)
+
+    # Check stmt order
+    stmt_ids_ordered = _get_runinstruction_ids_from_linearization(lin_items)
+    assert stmt_ids_ordered == stmt_ids_ordered_desired
+
+    # Check dep satisfaction
+    unsatisfied_deps = lp.find_unsatisfied_dependencies(proc_knl, lin_items)
+    assert not unsatisfied_deps
+
+    # }}}
 
 # }}}
 
