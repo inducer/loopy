0b88ff0851cf6cea0f62998145b91015f2b94f80
Mit Kotak <mitak2@illinois.edu>
2022-08-09 14:20:41 -0500

    changed to importing opencl_symbol_mangler

--- loopy/target/cuda.py
+++ loopy/target/cuda.py
@@ -297,32 +297,6 @@ def cuda_preamble_generator(preamble_info):
 
 # }}}
 
-# {{{ symbol mangler
-
-
-def cuda_symbol_mangler(kernel, name):
-    # FIXME: should be more picky about exact names
-    if name.startswith("FLT_"):
-        return NumpyType(np.dtype(np.float32)), name
-    elif name.startswith("DBL_"):
-        return NumpyType(np.dtype(np.float64)), name
-    elif name.startswith("M_"):
-        if name.endswith("_F"):
-            return NumpyType(np.dtype(np.float32)), name
-        else:
-            return NumpyType(np.dtype(np.float64)), name
-    elif name == "INFINITY":
-        return NumpyType(np.dtype(np.float32)), name
-    elif name.startswith("INT_"):
-        return NumpyType(np.dtype(np.int32)), name
-    elif name.startswith("LONG_"):
-        return NumpyType(np.dtype(np.int64)), name
-    elif name == "HUGE_VAL":
-        return NumpyType(np.dtype(np.float64)), name
-    else:
-        return None
-
-# }}
 
 # {{{ ast builder
 
@@ -340,9 +314,10 @@ class CudaCASTBuilder(CFamilyASTBuilder):
         return callables
 
     def symbol_manglers(self):
+        from loopy.target.opencl import opencl_symbol_mangler
         return (
                 super().symbol_manglers() + [
-                    cuda_symbol_mangler
+                    opencl_symbol_mangler
                     ])
     # }}}
 
