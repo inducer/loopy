d35f11b5fb10d58c0369ce3b0d47100fba4f5ab4
Addison J. Alvey-Blanco <aalveyblanco@gmail.com>
2022-11-05 20:04:45 -0500

    Changes in the way dependencies are computed

--- loopy/kernel/dependency.py
+++ loopy/kernel/dependency.py
@@ -9,21 +9,6 @@ from typing import Optional
 from dataclasses import dataclass
 from enum import Enum
 
-# TODO this is going away
-class DependencyType(Enum):
-    """An enumeration of the types of data dependencies found in a program.
-    """
-    WRITE_AFTER_READ  = 0
-    READ_AFTER_WRITE  = 1
-    WRITE_AFTER_WRITE = 2
-
-class AccessType(Enum):
-    """An enumeration of the types of accesses made by statements in a program.
-    """
-    READ  = 0
-    WRITE = 1
-
-# TODO this might be going away
 @dataclass(frozen=True)
 class HappensBefore: 
     """A class representing a "happens-before" relationship between two
@@ -42,18 +27,13 @@ class HappensBefore:
         An :class:`isl.Map` representing the data dependency. The input of the
         map is an iname tuple and the output of the map is a set of iname tuples
         that must execute after the input.
-
-    .. attribute:: dependency_type
-        A :class:`DependencyType` of :class:`Enum` representing the dependency
-        type (write-read, read-write, write-write). 
     """
     
     happens_before: str
     variable_name: Optional[str]
     relation: isl.Map
-    dependency_type: Optional[DependencyType]
 
-# TODO this is going away (probably)
+# TODO Do we really need this?
 @dataclass(frozen=True)
 class _AccessRelation:
     """A class that stores information about a particular array access in a
@@ -78,10 +58,8 @@ class _AccessRelation:
     id: str
     variable_name: str
     relation: isl.Map
-    access_type: AccessType
 
-def generate_dependency_relations(knl: LoopKernel) -> \
-        tuple[list[HappensBefore], list[HappensBefore], list[HappensBefore]]:
+def generate_dependency_relations(knl: LoopKernel) -> list[HappensBefore]:
     """Generates :class:`isl.Map` objects representing the data dependencies between
     statements in a loopy program. The :class:`isl.Map` objects are stored in a
     :class:`loopy.Dependency.HappensBefore` object along with the dependee id,
@@ -94,13 +72,16 @@ def generate_dependency_relations(knl: LoopKernel) -> \
     objects describing the data dependencies.
     """
     bmap: BatchedAccessMapMapper = BatchedAccessMapMapper(knl,
-                                                          knl.all_variable_names())
+                                      knl.all_variable_names())
     for insn in knl.instructions:
         bmap(insn.assignee, insn.within_inames)
         bmap(insn.expression, insn.within_inames)
 
     def get_map(var: str, insn: InstructionBase) -> isl.Map: 
         return bmap.access_maps[var][insn.within_inames]
+
+    def get_var_list(insn: InstructionBase) -> frozenset[str]:
+        return read_var_list(insn) | write_var_list(insn)
 
     def read_var_list(insn: InstructionBase) -> frozenset[str]:
         return insn.read_dependency_names() - insn.within_inames
@@ -115,47 +96,21 @@ def generate_dependency_relations(knl: LoopKernel) -> \
 
         return dependency
 
-    # TODO do not compute each dependency type separately, there is no need.
-    # instead, compute all dependencies at once so we do not need to unionize
-    # them later, i.e., in `generate_execution_order`.
+    # TODO can we do this without computing access maps and storing them?
+    accesses: list[_AccessRelation] = [_AccessRelation(insn.id, var,
+                                                       get_map(var, insn)) 
+                                       for insn in knl.instructions
+                                       for var in get_var_list(insn)]
 
-    read_maps: list[_AccessRelation] = [_AccessRelation(insn.id, var, 
-                                                      get_map(var, insn),
-                                                      AccessType.READ)
-                 for insn in knl.instructions
-                 for var in read_var_list(insn)]
-    write_maps: list[_AccessRelation] = [_AccessRelation(insn.id, var, 
-                                                      get_map(var, insn),
-                                                      AccessType.WRITE)
-                 for insn in knl.instructions
-                 for var in write_var_list(insn)]
+    dependencies: list[HappensBefore] = [HappensBefore(dependent.id,
+                                   dependee.variable_name,
+                                   get_dependency_relation(dependent.relation,
+                                                           dependee.relation))
+                         for dependent in accesses
+                         for dependee in accesses
+                         if dependent.variable_name == dependee.variable_name]
 
-    write_read: list[HappensBefore] = [HappensBefore(read.id,
-                                        write.variable_name,
-                                        get_dependency_relation(write.relation,
-                                                                read.relation),
-                                        DependencyType.WRITE_AFTER_READ)
-                                       for write in write_maps
-                                       for read in read_maps
-                                       if write.variable_name == read.variable_name]
-    read_write: list[HappensBefore] = [HappensBefore(write.id,
-                                        read.variable_name,
-                                        get_dependency_relation(read.relation,
-                                                                write.relation),
-                                        DependencyType.READ_AFTER_WRITE)
-                                       for read in read_maps
-                                       for write in write_maps
-                                       if read.variable_name == write.variable_name]
-    write_write: list[HappensBefore] = [HappensBefore(write2.id,
-                                         write1.variable_name,
-                                         get_dependency_relation(write1.relation,
-                                         write2.relation),
-                                         DependencyType.WRITE_AFTER_WRITE)
-                                        for write1 in write_maps
-                                        for write2 in write_maps
-                                        if write1.variable_name == write2.variable_name]
-
-    return write_read, read_write, write_write 
+    return dependencies  
 
 def generate_execution_order(knl: LoopKernel) -> frozenset[isl.Map]:
     """Generate the "happens-before" execution order that *must* be respected by
@@ -169,26 +124,17 @@ def generate_execution_order(knl: LoopKernel) -> frozenset[isl.Map]:
     execution required by the dependencies in a loopy program.
     """
 
-    write_read, read_write, write_write = generate_dependency_relations(knl)
-    
+    dependencies: list[HappensBefore] = generate_dependency_relations(knl)
     execution_order: frozenset[isl.Map] = frozenset()
-
     for insn in knl.instructions:
+        # TODO replace lexicographical domain with existing happens before
         domain: isl.BasicSet = knl.get_inames_domain(insn.within_inames)
-        insn_order: isl.Map = domain.lex_lt_set(domain)
-
-        union_of_dependencies: isl.Map = reduce(lambda x, 
-                                                y: x.relation | y.relation,
-                                                write_read)
-        union_of_dependencies |= reduce(lambda x, y: x.relation | y.relation,
-                                        read_write)
-        union_of_dependencies |= reduce(lambda x, y: x.relation | y.relation,
-                                        write_write)
-        
-        insn_order: isl.Map = insn_order & union_of_dependencies
+        insn_order: isl.Map = domain.lex_lt_set(domain) & \
+                              reduce(lambda x, y: x | y, [dep.relation for dep in
+                                                          dependencies])
         execution_order = execution_order | frozenset({insn_order})
 
-    return execution_order
+    return execution_order 
 
 def verify_execution_order(knl: LoopKernel, existing_happens_before: isl.Map):
     """Verify that a given transformation respects the dependencies in a
