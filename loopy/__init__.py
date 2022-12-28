66f27335999448a1c7fdb084213807bc925e5ffe
Isuru Fernando <isuruf@gmail.com>
2022-12-28 11:57:01 +0530

    Add unprivatize_temporaries_with_inames (#673)
    * Add unprivatize_temporaries_with_inames
    
    * Improve docstring and add roundtrip test
    
    * Fix test
    
    * Fix test again
    
    * Fix index in subscript
    
    * Improve error message
    
    * Fix base_indices in privatize too
    
    * Fix typo
    
    * loopy error msg

--- loopy/__init__.py
+++ loopy/__init__.py
@@ -115,7 +115,8 @@ from loopy.transform.padding import (
         find_padding_multiple,
         add_padding)
 
-from loopy.transform.privatize import privatize_temporaries_with_inames
+from loopy.transform.privatize import (privatize_temporaries_with_inames,
+        unprivatize_temporaries_with_inames)
 from loopy.transform.batch import to_batched
 from loopy.transform.parameter import assume, fix_parameters
 from loopy.transform.save import save_and_reload_temporaries
@@ -238,6 +239,7 @@ __all__ = [
         "find_padding_multiple", "add_padding",
 
         "privatize_temporaries_with_inames",
+        "unprivatize_temporaries_with_inames",
 
         "to_batched",
 
