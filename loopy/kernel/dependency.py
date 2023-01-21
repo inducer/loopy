51c460d70faae119f20888d1de84a784d5b7d4fd
Addison J. Alvey-Blanco <aalveyblanco@gmail.com>
2023-01-21 16:06:37 -0600

    Add relation to HappensAfter data structure

--- loopy/kernel/dependency.py
+++ loopy/kernel/dependency.py
@@ -1,12 +1,31 @@
-# FIXME Add copyright header
+__copyright__ = "Copyright (C) 2023 Addison Alvey-Blanco"
 
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 
 import islpy as isl
 from islpy import dim_type
+
 import pymbolic.primitives as p
 
 from dataclasses import dataclass
-from islpy import Map
 from typing import Optional
 
 from loopy import LoopKernel
@@ -17,7 +36,7 @@ from loopy.typing import ExpressionT
 @dataclass(frozen=True)
 class HappensAfter:
     variable_name: Optional[str]
-    instances_rel: Optional[Map]
+    instances_rel: Optional[isl.Map]
 
 class AccessMapMapper(WalkMapper):
     """
@@ -116,112 +135,109 @@ def compute_happens_after(knl: LoopKernel) -> LoopKernel:
     # return the kernel with the new instructions
     return knl.copy(instructions=new_insns)
 
-def add_lexicographic_happens_after_orig(knl: LoopKernel) -> None:
-    """
-    TODO properly format this documentation.
-
-    Creates a dependency relation between two instructions based on a
-    lexicographic ordering of the statements in a program.
-
-    For example, the C-like execution order (i.e. sequential ordering) of a
-    program.
-    """
-
-    # we want to modify the output dimension and OUT = 3
-    dim_type = isl.dim_type.out
-
-    # generate an unordered mapping from statement instances to points in the
-    # loop domain
-    insn_number = 0
-    schedules = {}
-    for insn in knl.instructions:
-        domain = knl.get_inames_domain(insn.within_inames)
-
-        # if we do not set the dim name, the name is set as None
-        domain = domain.insert_dims(dim_type, 0, 1).set_dim_name(dim_type, 0,
-                                                                 insn.id)
-
-        space = domain.get_space()
-        domain = domain.add_constraint(
-            isl.Constraint.eq_from_names(space, {1: -1*insn_number, insn.id: 1})
-        )
-
-        # this may not be the final way we keep track of the schedules
-        schedule = isl.Map.from_domain_and_range(domain, domain)
-        schedules[insn.id] = schedule
-
-        insn_number += 1
-
-        # determine a lexicographic order on the space the schedules belong to
-
-
 @for_each_kernel
 def add_lexicographic_happens_after(knl: LoopKernel) -> LoopKernel:
+    """
+    TODO update documentation to follow the proper format
+
+    Determine a coarse "happens-before" relationship between an instruction and
+    the instruction immediately preceeding it. This strict execution order is
+    relaxed in accordance with the data dependency relations.
+
+    See `loopy.dependency.compute_happens_after` for data dependency generation.
+    """
 
     new_insns = []
 
     for iafter, insn_after in enumerate(knl.instructions):
+
+        # the first instruction does not have anything preceding it
         if iafter == 0:
             new_insns.append(insn_after)
+        
+        # all other instructions "happen after" the instruction before it
         else:
+
+            # not currently used
+            # unshared_before = insn_before.within_inames
+            
+            # get information about the preceding instruction
             insn_before = knl.instructions[iafter - 1]
             shared_inames = insn_after.within_inames & insn_before.within_inames
-            unshared_before = insn_before.within_inames
 
+            # generate a map from the preceding insn to the current insn
             domain_before = knl.get_inames_domain(insn_before.within_inames)
             domain_after = knl.get_inames_domain(insn_after.within_inames)
-
             happens_before = isl.Map.from_domain_and_range(
-                    domain_before, domain_after)
+                    domain_before, domain_after
+            )
+
+            # update inames so they are unique 
             for idim in range(happens_before.dim(dim_type.out)):
                 happens_before = happens_before.set_dim_name(
                         dim_type.out, idim,
                         happens_before.get_dim_name(dim_type.out, idim) + "'")
+            
+            # generate a set containing all inames (from both domains)
             n_inames_before = happens_before.dim(dim_type.in_)
             happens_before_set = happens_before.move_dims(
                     dim_type.out, 0,
                     dim_type.in_, 0,
                     n_inames_before).range()
 
+            # verify the order of the inames
             shared_inames_order_before = [
                     domain_before.get_dim_name(dim_type.out, idim)
                     for idim in range(domain_before.dim(dim_type.out))
                     if domain_before.get_dim_name(dim_type.out, idim)
-                    in shared_inames]
+                    in shared_inames
+                    ]
             shared_inames_order_after = [
                     domain_after.get_dim_name(dim_type.out, idim)
                     for idim in range(domain_after.dim(dim_type.out))
                     if domain_after.get_dim_name(dim_type.out, idim)
-                    in shared_inames]
-
+                    in shared_inames
+                    ]
             assert shared_inames_order_after == shared_inames_order_before
             shared_inames_order = shared_inames_order_after
 
+            # generate lexicographical map from space of preceding to current insn
             affs = isl.affs_from_space(happens_before_set.space)
 
+            # start with an empty set
             lex_set = isl.Set.empty(happens_before_set.space)
             for iinnermost, innermost_iname in enumerate(shared_inames_order):
+                
                 innermost_set = affs[innermost_iname].lt_set(
-                        affs[innermost_iname+"'"])
-
+                        affs[innermost_iname+"'"]
+                )
+                
                 for  outer_iname in shared_inames_order[:iinnermost]:
                     innermost_set = innermost_set & (
-                            affs[outer_iname].eq_set(affs[outer_iname + "'"]))
+                            affs[outer_iname].eq_set(affs[outer_iname + "'"])
+                            )
 
+                # update the set
                 lex_set = lex_set | innermost_set
 
+            # create the map
             lex_map = isl.Map.from_range(lex_set).move_dims(
                     dim_type.in_, 0,
                     dim_type.out, 0,
                     n_inames_before)
 
+            # update happens_before map
             happens_before = happens_before & lex_map
 
-            pu.db
+            # create HappensAfter and add the relation to it
+            new_happens_after = { 
+                insn_before.id: HappensAfter(None, happens_before) 
+            }
 
+            insn_after = insn_after.copy(happens_after=new_happens_after)
+
+            # update instructions
             new_insns.append(insn_after)
 
     return knl.copy(instructions=new_insns)
-
-
 
