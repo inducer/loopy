4c7440c1318b2e67f9217b39d2c9a6f2adda0e20
Connor Ward <c.ward20@imperial.ac.uk>
2023-05-04 09:49:06 +0100

    Do not attempt op counting for CInstructions

--- loopy/statistics.py
+++ loopy/statistics.py
@@ -1676,7 +1676,7 @@ def _get_op_map_for_single_kernel(knl, callables_table,
 
     for insn in knl.instructions:
         if within(knl, insn):
-            if isinstance(insn, (CallInstruction, CInstruction, Assignment)):
+            if isinstance(insn, (CallInstruction, Assignment)):
                 ops = op_counter(insn.assignees) + op_counter(insn.expression)
                 for key, val in ops.count_map.items():
                     count = _get_insn_count(knl, callables_table, insn.id,
@@ -1684,7 +1684,7 @@ def _get_op_map_for_single_kernel(knl, callables_table,
                                 key.count_granularity)
                     op_map = op_map + ToCountMap({key: val}) * count
 
-            elif isinstance(insn, (NoOpInstruction, BarrierInstruction)):
+            elif isinstance(insn, (CInstruction, NoOpInstruction, BarrierInstruction)):
                 pass
             else:
                 raise NotImplementedError("unexpected instruction item type: '%s'"
