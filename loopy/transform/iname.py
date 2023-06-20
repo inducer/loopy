12ccc0d328e3bc5cb74d06f4bd2ef535179eb957
Nicholas Christensen <njchris2@illinois.edu>
2023-06-19 22:10:00 -0500

    flake8 fix

--- loopy/transform/iname.py
+++ loopy/transform/iname.py
@@ -2501,6 +2501,7 @@ def rename_inames(kernel, old_inames, new_iname, existing_ok=False,
 
     return kernel
 
+
 @for_each_kernel
 def rename_iname(kernel, old_iname, new_iname, existing_ok=False,
                  within=None, preserve_tags=True):
