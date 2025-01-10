a5bd5c1250e39e60ce4b4354ff56a2c3d65c6ccb
Nick Koskelo <koskelo2@illinois.edu>
2025-01-10 18:18:55 +0000

    Update example.

--- examples/python/loop_unroll_codegen.py
+++ examples/python/loop_unroll_codegen.py
@@ -24,17 +24,19 @@ knl = lp.make_kernel(
 knl = lp.tag_inames(knl, {"i": "vec"})
 from loopy.kernel.array import VectorArrayDimTag
 
+
 try:
     orig_knl = knl
     knl = lp.tag_array_axes(knl, "out", [VectorArrayDimTag()])
-    knl = lp.add_and_infer_dtypes(knl, {"a": np.float32, "b": np.float32, "c": np.float32})
+    knl = lp.add_and_infer_dtypes(knl, {"a": np.float32,
+                                        "b": np.float32,
+                                        "c": np.float32})
 
     dev_code = lp.generate_code_v2(knl).device_code()
     print(dev_code)
 
 except Exception as err:
     print(err)
-breakpoint()
 
 print("No Vector Array Tag.")
 knl = orig_knl
@@ -46,7 +48,3 @@ knl = lp.tag_inames(knl, {"i": "ilp.unr"})
 knl = lp.add_and_infer_dtypes(knl, {"a": np.float32, "b": np.float32, "c": np.float32})
 dev_code = lp.generate_code_v2(knl).device_code()
 print(dev_code)
-breakpoint()
-
-print("Hello")
-
