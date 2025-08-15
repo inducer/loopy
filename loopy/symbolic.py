8e317ce8d29aacfbe7fa6bc6ef8144090f7c9bad
Matthias Diener <mdiener@illinois.edu>
2025-08-15 11:03:27 -0500

    ruff

--- loopy/symbolic.py
+++ loopy/symbolic.py
@@ -567,7 +567,7 @@ class DependencyMapper(DependencyMapperBase[P]):
         return set()
 
     def map_tagged_expression(self, expr: TaggedExpression, *args: P.args,
-                              **kwargs: P.kwargs) -> DependenciesT:
+                              **kwargs: P.kwargs) -> Dependencies:
         return self.rec(expr.expr, *args, **kwargs)
 
     def map_sub_array_ref(self, expr, *args: P.args, **kwargs: P.kwargs):
